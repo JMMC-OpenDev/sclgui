11059bdd157049ffe7f24a16bf9a57689a672ad4
gzins <gzins@jmmc.fr>
2004-07-26 07:41:53 +0000

    Created from vobsCALIBRATOR_STAR_LIST

---
+++ vobs/include/vobsSTAR_LIST.h
@@ -0,0 +1,65 @@
+#ifndef vobsSTAR_LIST_H
+#define vobsSTAR_LIST_H
+/*******************************************************************************
+* JMMC project
+*
+* "@(#) $Id: vobsSTAR_LIST.h,v 1.1 2004-07-26 07:41:53 gzins Exp $"
+*
+* who       when         what
+* --------  -----------  -------------------------------------------------------
+* scetre    06-Jul-2004  Created
+*
+*
+*******************************************************************************/
+
+#ifndef __cplusplus
+#error This is a C++ include file and cannot be used from plain C
+#endif
+
+/*
+ * header files
+ */
+#include<list>
+#include"vobsSTAR.h"
+
+/*
+ * constants
+ */
+/*none*/
+
+class vobsSTAR_LIST
+{ 
+ public:
+    vobsSTAR_LIST();//constructor
+    virtual ~vobsSTAR_LIST();//destructor
+   
+    virtual mcsLOGICAL    IsEmpty(void);
+    virtual mcsCOMPL_STAT Clear(void);
+    virtual mcsCOMPL_STAT AddAtTail(vobsSTAR &star);
+    virtual mcsCOMPL_STAT Remove(vobsSTAR &star);
+    virtual mcsUINT32     Size(void);
+
+    virtual vobsSTAR      *GetNextStar(mcsLOGICAL init = mcsFALSE);
+    virtual vobsSTAR      *GetStar(vobsSTAR &star);
+    
+    virtual mcsCOMPL_STAT Merge(vobsSTAR_LIST &list);
+
+    void Display(void);
+
+ protected:
+
+ private:
+    // Declaration of copy constructor and assignment operator as private
+    // methods, in order to hide them from the users.
+    vobsSTAR_LIST& operator=(const vobsSTAR_LIST&);
+    vobsSTAR_LIST (const vobsSTAR_LIST&);
+
+    // List of stars
+    std::list<vobsSTAR>           _starList;
+    std::list<vobsSTAR>::iterator _starIterator;
+};
+
+#endif /*!vobSTAR_LIST_H*/
+
+
+/*___oOo___*/
