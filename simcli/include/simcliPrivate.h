fae2dd4761c2b735cdaf31848b47c3256b189262
JMMC-OpenDev <jmmc-tech-group@jmmc.fr>
2004-11-16 17:10:02 +0000

    Fix directory structure and add additional files

---
+++ simcli/include/simcliPrivate.h
@@ -0,0 +1,43 @@
+#ifndef simcliPrivate_H
+#define simcliPrivate_H
+/*******************************************************************************
+* JMMC project
+*
+* "@(#) $Id: simcliPrivate.h,v 1.1 2004-11-16 17:10:02 sccmgr Exp $"
+*
+* who       when         what
+* --------  -----------  -------------------------------------------------------
+* sccmgr    16-Nov-2004  Created
+*
+*
+*******************************************************************************/
+
+/**
+ * \file
+ * Brief description of the header file, which ends at this dot.
+ */
+
+/* The following piece of code alternates the linkage type to C for all 
+functions declared within the braces, which is necessary to use the 
+functions in C++-code.
+*/
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/*
+ * Module name
+ */
+#define MODULE_ID "simcli"
+
+ 
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif /*!simcliPrivate_H*/
+
+/*___oOo___*/
