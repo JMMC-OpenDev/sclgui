ea41700bb5026a6b94f64046973991986ddb969a
JMMC-OpenDev <jmmc-tech-group@jmmc.fr>
2004-11-23 09:25:09 +0000

    Fix directory structure and add additional files

---
+++ sclgui/include/sclguiPrivate.h
@@ -0,0 +1,43 @@
+#ifndef sclguiPrivate_H
+#define sclguiPrivate_H
+/*******************************************************************************
+* JMMC project
+*
+* "@(#) $Id: sclguiPrivate.h,v 1.1 2004-11-23 09:22:15 sccmgr Exp $"
+*
+* who       when         what
+* --------  -----------  -------------------------------------------------------
+* sccmgr    23-Nov-2004  Created
+*
+*
+*******************************************************************************/
+
+/**
+ * \file
+ * Brief description of the header file, which ends at this dot.
+ */
+
+/* The following piece of code alternates the linkage type to C for all 
+functions declared within the braces, which is necessary to use the 
+functions in C++-code.
+*/
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/*
+ * Module name
+ */
+#define MODULE_ID "sclgui"
+
+ 
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif /*!sclguiPrivate_H*/
+
+/*___oOo___*/
---
+++ sclsvr/include/sclsvrPrivate.h
@@ -0,0 +1,43 @@
+#ifndef sclsvrPrivate_H
+#define sclsvrPrivate_H
+/*******************************************************************************
+* JMMC project
+*
+* "@(#) $Id: sclsvrPrivate.h,v 1.1 2004-11-23 09:21:10 sccmgr Exp $"
+*
+* who       when         what
+* --------  -----------  -------------------------------------------------------
+* sccmgr    23-Nov-2004  Created
+*
+*
+*******************************************************************************/
+
+/**
+ * \file
+ * Brief description of the header file, which ends at this dot.
+ */
+
+/* The following piece of code alternates the linkage type to C for all 
+functions declared within the braces, which is necessary to use the 
+functions in C++-code.
+*/
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/*
+ * Module name
+ */
+#define MODULE_ID "sclsvr"
+
+ 
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif /*!sclsvrPrivate_H*/
+
+/*___oOo___*/
