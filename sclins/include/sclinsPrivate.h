6e288be82521496d1e5c64993f013e4bcd8f8e36
JMMC-OpenDev <jmmc-tech-group@jmmc.fr>
2004-12-08 06:18:42 +0000

    Fix directory structure and add additional files

---
+++ sclins/include/sclinsPrivate.h
@@ -0,0 +1,43 @@
+#ifndef sclinsPrivate_H
+#define sclinsPrivate_H
+/*******************************************************************************
+* JMMC project
+*
+* "@(#) $Id: sclinsPrivate.h,v 1.1 2004-12-08 06:18:42 sccmgr Exp $"
+*
+* who       when         what
+* --------  -----------  -------------------------------------------------------
+* sccmgr    08-Dec-2004  Created
+*
+*
+*******************************************************************************/
+
+/**
+ * \file
+ * Brief description of the header file, which ends at this dot.
+ */
+
+/* The following piece of code alternates the linkage type to C for all 
+functions declared within the braces, which is necessary to use the 
+functions in C++-code.
+*/
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/*
+ * Module name
+ */
+#define MODULE_ID "sclins"
+
+ 
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif /*!sclinsPrivate_H*/
+
+/*___oOo___*/
