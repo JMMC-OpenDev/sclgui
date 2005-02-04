c5baf1409b0e9aabf4b93838b1ec03b87b17a0e0
gzins <gzins@jmmc.fr>
2005-02-04 09:54:42 +0000

    Created

---
+++ vobs/src/vobsCATALOG_MIDI.cpp
@@ -0,0 +1,452 @@
+/*******************************************************************************
+ * JMMC project
+ *
+ * "@(#) $Id: vobsCATALOG_MIDI.cpp,v 1.1 2005-02-04 09:54:42 gzins Exp $"
+ *
+ * History
+ * -------
+ * $Log: not supported by cvs2svn $
+ ******************************************************************************/
+
+/**
+ * \file
+ *  Definition of vobsCATALOG_MIDI class.
+ */
+
+static char *rcsId="@(#) $Id: vobsCATALOG_MIDI.cpp,v 1.1 2005-02-04 09:54:42 gzins Exp $"; 
+static void *use_rcsId = ((void)&use_rcsId,(void *) &rcsId);
+
+/* 
+ * System Headers 
+ */
+#include <iostream>
+using namespace std;
+
+/*
+ * MCS Headers 
+ */
+#include "mcs.h"
+#include "log.h"
+#include "err.h"
+#include "math.h"
+
+/*
+ * Local Headers 
+ */
+#include "vobsCATALOG_MIDI.h"
+#include "vobsPrivate.h"
+#include "vobsErrors.h"
+#include "vobsCDATA.h"
+#include "vobsPARSER.h"
+
+/**
+ * Class constructor
+ */
+vobsCATALOG_MIDI::vobsCATALOG_MIDI() : _catalogFilename("vobMidiCatalog.cfg")
+{
+    // Set catalog name
+    SetName("MIDI");
+}
+
+/**
+ * Class destructor
+ */
+vobsCATALOG_MIDI::~vobsCATALOG_MIDI()
+{
+}
+
+/*
+ * Public methods
+ */
+
+/**
+ * Search for star list in MIDI catalog.
+ *
+ * Build star list from MIDI catalog, where each list star verifies constraints
+ * specified by user request.
+ *
+ * \param request search contraints
+ * \param list star list provided by the search
+ *
+ * \return mcsSUCCESS on successful completion. Otherwise mcsFAILURE is returned.
+ * 
+ * \sa vobsREQUEST class
+ *
+ * \b Errors codes:\n 
+ * The possible errors are:
+ */
+mcsCOMPL_STAT vobsCATALOG_MIDI::Search(vobsREQUEST &request,
+                                       vobsSTAR_LIST &list)
+{
+    logExtDbg("vobsCATALOG_MIDI::Search()");
+
+    //
+    // Load MIDI catalog in star list
+    // ------------------------------
+    if (Load() == mcsFAILURE)
+    {
+        // Get catalog name
+        mcsSTRING256 catalogName;
+        if (GetName(catalogName) == mcsFAILURE)
+        {
+            return mcsFAILURE;
+        }
+
+        errAdd(vobsERR_CATALOG_LOAD, catalogName);
+        return mcsFAILURE;
+    }
+    logDebug("MIDI catalog is correctly loaded in a star list");
+    
+    //
+    // Build reference (science) object
+    // --------------------------------
+
+    // Get reference object properties
+    mcsSTRING256 ra;            // reference object right ascension
+    mcsSTRING256 dec;           // reference object declinaison
+    mcsFLOAT magnitude;         // reference object magnitude
+
+    // ra
+    if (request.GetConstraint(RA_ID, ra) == mcsFAILURE)
+    {
+        return mcsFAILURE;
+    }
+    printf("ra = %s\n", ra);
+    
+    // dec
+    if (request.GetConstraint(DEC_ID, dec) == mcsFAILURE)
+    {
+        return mcsFAILURE;
+    }
+    printf("dec = %s\n", dec);
+    
+    // magnitude
+    if (request.GetConstraint(STAR_MAGNITUDE_ID, &magnitude) == mcsFAILURE)
+    {
+        return mcsFAILURE;
+    }
+    printf("magnitude = %f\n", magnitude);
+    
+    // Create the reference star
+    vobsSTAR referenceStar;
+
+    // Add reference star properties
+    // ra
+    if (referenceStar.SetPropertyValue("POS_EQ_RA_MAIN", ra) == mcsFAILURE)
+    {
+        return mcsFAILURE;
+    }
+    // dec
+    if (referenceStar.SetPropertyValue("POS_EQ_DEC_MAIN", dec) == mcsFAILURE)
+    {
+        return mcsFAILURE;
+    }
+    // N magnitude
+    if (referenceStar.SetPropertyValue("PHOT_JHN_N", magnitude) == mcsFAILURE)
+    {
+        return mcsFAILURE;
+    }
+
+    //
+    // Build constraint list
+    // ---------------------
+    // Aim is to set search field
+    
+    // Get reference object constraints
+    mcsINT32 diffRa;    // reference object ra constaint
+    mcsINT32 diffDec;   // reference object dec constaint
+    // ra constraint
+    if (request.GetConstraint(SEARCH_BOX_RA_ID, &diffRa) == mcsFAILURE)
+    {
+        return mcsFAILURE;
+    }
+    // dec constraint
+    if (request.GetConstraint(SEARCH_BOX_DEC_ID, &diffDec) == mcsFAILURE)
+    {
+        return mcsFAILURE;
+    }
+    
+    // Create reference object constraint list
+    vobsSTAR_COMP_CRITERIA_LIST constraintlist;
+    
+    // Add reference star constraints to constaint list
+    // Field on sky: ra constraint
+    if (constraintlist.Add("POS_EQ_RA_MAIN", diffRa) == mcsFAILURE)
+    {
+        return mcsFAILURE;
+    }
+    // Field on sky: dec constraint
+    if (constraintlist.Add("POS_EQ_DEC_MAIN", diffDec) == mcsFAILURE)
+    {
+        return mcsFAILURE;
+    }
+
+    // Magnitude range constraint
+    // Object magnitude conversion to IRAS flux 12mu
+    mcsFLOAT referenceStarFlux;
+    mcsFLOAT minNFlux;
+    mcsFLOAT maxNFlux;
+    mcsFLOAT diffNFlux;
+    mcsFLOAT middleNFlux;
+    referenceStarFlux = 0.89 * pow(10, -0.4 * (magnitude - 4.1));
+
+    // Calculate N magnitude range constraint depending on object flux
+    // N magnitude range constraint is half N magnitude range
+    minNFlux = 5;
+    if (referenceStarFlux < 10)
+    {
+        maxNFlux = 20;
+    }
+    else if (referenceStarFlux >= 10 && referenceStarFlux <= 100)
+    {
+        maxNFlux = 50;
+    }
+    else
+    {
+        // referenceStarFlux > 100
+        maxNFlux = 100;
+    }
+    diffNFlux = (maxNFlux - minNFlux) / 2;
+    middleNFlux = (minNFlux + diffNFlux);
+
+    // Add N flux constraint to constraint list
+    if (constraintlist.Add("PHOT_FLUX_IR_12", diffNFlux) == mcsFAILURE)
+    {
+        return mcsFAILURE;
+    }
+    
+    // Set flux for reference star to the middle of the flux range
+    if (referenceStar.SetPropertyValue("PHOT_FLUX_IR_12", 
+                                       middleNFlux) == mcsFAILURE)
+    {
+        return mcsFAILURE;
+    }
+
+    //
+    // Select MIDI catalog stars which verifies constraints
+    // ----------------------------------------------------
+
+    for (mcsUINT32 i=0; i<_starList.Size(); i++)
+    {
+        // Get MIDI catalog star
+        vobsSTAR *midiCatalogStar;
+        midiCatalogStar = _starList.GetNextStar((mcsLOGICAL)(i==0));
+        // Compare MIDI catalog star with reference star
+        if (midiCatalogStar->IsSame(referenceStar, &constraintlist) == mcsTRUE)
+        {
+            // If Compare MIDI catalog star verifies constraint list then add it
+            // to the resulting list
+            list.AddAtTail(*midiCatalogStar);
+        }
+    }
+
+    return mcsSUCCESS;
+}
+
+
+/*
+ * Protected methods
+ */
+
+
+/*
+ * Private methods
+ */
+
+/**
+ * Load MIDI catalog.
+ *
+ * Build star list from MIDI catalog stars.
+ *
+ * \return mcsSUCCESS on successful completion. Otherwise mcsFAILURE is returned.
+ * 
+ * \sa vobsCATALOG class
+ *
+ * \b Errors codes:\n 
+ * The possible errors are:
+ */
+mcsCOMPL_STAT vobsCATALOG_MIDI::Load(void)
+{
+    logExtDbg("vobsCATALOG_MIDI::Load()");
+    
+    //
+    // Load MIDI catalog into a buffer
+    // -------------------------------
+    logDebug("Load MIDI catalog ...");
+
+    // Catalog has already been loaded
+    if (_loaded == mcsTRUE)
+    {
+        return mcsSUCCESS;
+    }
+
+    // Catalog has not already been loaded
+    
+    // Search for file location
+    const char *catalogFileName;
+    catalogFileName = miscLocateFile(_catalogFilename);
+    if (catalogFileName == NULL)
+    {
+        return mcsFAILURE;
+    }
+
+    // Resolve environment variables (if any)
+    catalogFileName = miscResolvePath(catalogFileName);
+    if (catalogFileName == NULL)
+    {
+        return mcsFAILURE;
+    }
+    
+    // Init buffer to load catalog file
+    miscDYN_BUF dynBuf;
+    miscDynBufInit(&dynBuf);
+
+    // Load catalog file except comment lines beginning by '#'
+    if (miscDynBufLoadFile(&dynBuf, catalogFileName, "#") == mcsFAILURE)
+    {
+        miscDynBufDestroy(&dynBuf);
+        return mcsFAILURE;
+    }
+
+    
+    //
+    // Load CDATA structure with MIDI stars
+    // ------------------------------------
+    logDebug("Load CDATA structure ...");
+
+    // Create an object CDATA to transfer MIDI catalog from the dynamic
+    // buffer
+    vobsCDATA cDataStructure;
+    
+    // Set line to skip to -1 because in the CDATA appendLines method, number of
+    // lines to skip is increased of 1, because there is an empty line between
+    // each data line (due to \n to \0 conversion). The CDATA appendLines method
+    // will have to be changed for this reason.
+    cDataStructure.setNbLinesToSkip(-1);
+
+    // For each dynamic buffer line
+    int  lineNb = 1;
+    char *line = NULL;
+    // While it's not the end of file
+    while ((line = miscDynBufGetNextLine(&dynBuf, line, mcsTRUE)) != NULL)
+    {
+        // Trim leading and trailing spaces from the line
+        miscTrimString (line, " ");
+        
+        // If line is not empty
+        if (strlen(line) != 0)
+        {
+            if (lineNb == 1)
+            {
+                // It's the first line: UCD name line
+                // Parse UCD name line and strore them in an array
+                mcsSTRING256 ucdNameArray[25];
+                mcsUINT32 nbOfUcdName;
+                if (miscSplitString(line, '\t', ucdNameArray, 
+                                    25, &nbOfUcdName) == mcsFAILURE)
+                {
+                    errAdd(vobsERR_INVALID_PARSING_UCD_FORMAT);
+                    
+                    miscDynBufDestroy(&dynBuf);
+                    return mcsFAILURE;
+                }
+                // For each UCD name stored in the array
+                logDebug("\t-> Add UCD name to CDATA structure ...");
+                logDebug("\t\tNumber of UCD name = %i", nbOfUcdName);
+                for (mcsUINT32 i=0; i<nbOfUcdName; i++)
+                {
+                    // add UCD name to CDATA structure
+                    logDebug("\t\t%d- %s\n", i, ucdNameArray[i]);
+                    if (cDataStructure.addUcdName(ucdNameArray[i]) == 
+                                                                    mcsFAILURE)
+                    {
+                        // Do not raise an error because the method returns
+                        // always SUCCESS
+                        return mcsFAILURE;
+                    }
+                }
+            }
+            else if (lineNb == 2)
+            {
+                // It's the second line: UCD column name line
+                // Parse UCD column name line and strore them in an array
+                mcsSTRING256 ucdColNameArray[25];
+                mcsUINT32 nbOfUcdColName;
+                if (miscSplitString(line, '\t', ucdColNameArray, 
+                                    25, &nbOfUcdColName) == mcsFAILURE)
+                {
+                    errAdd(vobsERR_INVALID_PARSING_UCD_FORMAT);
+
+                    miscDynBufDestroy(&dynBuf);
+                    return mcsFAILURE;
+                }
+                // For each UCD column description stored in the array
+                logDebug("\t-> Add UCD column name to CDATA structure ...");
+                logDebug("\t\tNumber of UCD column name = %i", \
+                         nbOfUcdColName);
+                for (mcsUINT32 i=0; i<nbOfUcdColName; i++)
+                {
+                    // add UCD colomn name to CDATA structure
+                    logDebug("\t\t%d- %s\n", i, ucdColNameArray[i]);
+                    if (cDataStructure.addColName(ucdColNameArray[i]) == 
+                        mcsFAILURE)
+                    {
+                        // Do not raise an error because the method returns
+                        // always SUCCESS
+                        return mcsFAILURE;
+                    }
+                }
+            }
+            else
+            {
+                // It's neither the first line the second one: CDATA line
+                if (cDataStructure.appendLines(line) == mcsFAILURE)
+                {
+                    errAdd(vobsERR_APPEND_LINE_FAILED, line);
+                    return mcsFAILURE;
+                }
+            }
+            
+            /* Next line */
+            lineNb++;
+       }
+    }
+    
+    // Return to 0 line to skip (see previous explanation)
+    cDataStructure.setNbLinesToSkip(0);    
+    
+    // Destroy dynamic buffer
+    miscDynBufDestroy(&dynBuf);
+
+    //
+    // Parse CDATA and set star list
+    // -----------------------------
+    logDebug("Parse CDATA and build star list ...");
+    
+    // Create a parser to parse CDATA
+    vobsPARSER parser;
+    // Parse CDATA
+    if (parser.ParseCData(&cDataStructure, _starList) == mcsFAILURE)
+    {
+        errAdd(vobsERR_PARSE_CDATA);
+    }
+
+    // If log level is DEBUG or EXTDBG
+    if (logGetStdoutLogLevel() >= logDEBUG)
+    {
+        if (_starList.Save("$MCSDATA/tmp/catalogMIDI.dat") == mcsFAILURE)
+        {
+            // Ignore error (for test only)
+            errCloseStack();
+        }
+    }
+
+    // Set flag indicating a correct catalog load
+    _loaded = mcsTRUE;
+    
+    return mcsSUCCESS;
+}
+
+
+
+/*___oOo___*/
