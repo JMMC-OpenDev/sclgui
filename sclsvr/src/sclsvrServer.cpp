4a436a11290a003b65ec12511a50e67dfa25be74
gzins <gzins@jmmc.fr>
2005-02-08 04:52:33 +0000

    Printed ou error stack when connection to MCS services fails

--- sclsvr/src/sclsvrServer.cpp
+++ sclsvr/src/sclsvrServer.cpp
@@ -1,11 +1,14 @@
 /*******************************************************************************
  * JMMC project
  *
- * "@(#) $Id: sclsvrServer.cpp,v 1.6 2005-02-07 15:01:21 gzins Exp $"
+ * "@(#) $Id: sclsvrServer.cpp,v 1.7 2005-02-08 04:52:33 gzins Exp $"
  *
  * History
  * -------
  * $Log: not supported by cvs2svn $
+ * Revision 1.6  2005/02/07 15:01:21  gzins
+ * Added CVS log as modification history
+ *
  * gzins     23-Nov-2004  Created
  *
  ******************************************************************************/
@@ -15,7 +18,7 @@
  * Search Calibrators SERVER
  */
 
-static char *rcsId="@(#) $Id: sclsvrServer.cpp,v 1.6 2005-02-07 15:01:21 gzins Exp $"; 
+static char *rcsId="@(#) $Id: sclsvrServer.cpp,v 1.7 2005-02-08 04:52:33 gzins Exp $"; 
 static void *use_rcsId = ((void)&use_rcsId,(void *) &rcsId);
 
 
@@ -57,6 +60,7 @@ int main(int argc, char *argv[])
     // Init server
     if (scalibServer.Init(argc, argv) == mcsFAILURE)
     {
+        errCloseStack();
         exit (EXIT_FAILURE);
     }
 
