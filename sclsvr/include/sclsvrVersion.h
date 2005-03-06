44f64add1e92783aad50d502d6721e245bf6c01a
gzins <gzins@jmmc.fr>
2005-03-06 20:26:55 +0000

    Created

---
+++ sclsvr/include/sclsvrVersion.h
@@ -0,0 +1,36 @@
+#ifndef sclsvrVersion_H
+#define sclsvrVersion_H
+/*******************************************************************************
+ * JMMC project
+ *
+ * "@(#) $Id: sclsvrVersion.h,v 1.1 2005-03-06 20:26:55 gzins Exp $"
+ *
+ * History
+ * -------
+ * $Log: not supported by cvs2svn $
+ ******************************************************************************/
+
+/**
+ * \file
+ * Definition of the software version.
+ */
+
+/* The following piece of code alternates the linkage type to C for all 
+functions declared within the braces, which is necessary to use the 
+functions in C++-code.
+*/
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Software version 
+#define sclsvrVERSION "3.0 Beta"
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif /*!sclsvrVersion_H*/
+
+/*___oOo___*/
