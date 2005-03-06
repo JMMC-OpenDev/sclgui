13cb79fa54641a6f41c0406657cbff4c0691cd31
gzins <gzins@jmmc.fr>
2005-03-06 20:28:24 +0000

    Added sclsvrREQUEST_TAG and sclsvrFORMAT_TAG definition

--- sclsvr/include/sclsvrPrivate.h
+++ sclsvr/include/sclsvrPrivate.h
@@ -3,11 +3,14 @@
 /*******************************************************************************
  * JMMC project
  *
- * "@(#) $Id: sclsvrPrivate.h,v 1.3 2005-02-07 15:00:13 gzins Exp $"
+ * "@(#) $Id: sclsvrPrivate.h,v 1.4 2005-03-06 20:28:24 gzins Exp $"
  *
  * History
  * -------
  * $Log: not supported by cvs2svn $
+ * Revision 1.3  2005/02/07 15:00:13  gzins
+ * Added CVS log as modification history
+ *
  * sccmgr    23-Nov-2004  Created
  *
  ******************************************************************************/
@@ -31,7 +34,11 @@ extern "C" {
  */
 #define MODULE_ID "sclsvr"
 
- 
+// Tag used to stored request into file
+#define sclsvrREQUEST_TAG "# REQUEST = "
+
+// Tag used to stored format into file
+#define sclsvrFORMAT_TAG  "# FORMAT = "
 
 #ifdef __cplusplus
 }
