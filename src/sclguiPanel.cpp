8b1724234cf66273ecc85fbdb9476c1c71471c34
gzins <gzins@jmmc.fr>
2005-03-08 16:33:58 +0000

    Disconnected process from message service before exiting

--- src/sclguiPanel.cpp
+++ src/sclguiPanel.cpp
@@ -1,11 +1,15 @@
 /*******************************************************************************
  * JMMC project
  *
- * "@(#) $Id: sclguiPanel.cpp,v 1.6 2005-03-07 17:50:03 gzins Exp $"
+ * "@(#) $Id: sclguiPanel.cpp,v 1.7 2005-03-08 16:33:58 gzins Exp $"
  *
  * History
  * -------
  * $Log: not supported by cvs2svn $
+ * Revision 1.6  2005/03/07 17:50:03  gzins
+ * Improved star number check for delete callback: check whether is an interger or not
+ * Changed messages intended to end user to be homogeneous and more explicit
+ *
  * Revision 1.5  2005/03/06 10:51:00  gzins
  * Removed debug messages on cout
  *
@@ -21,7 +25,7 @@
  * Entry point for SearchCalib graphical application. 
  */
 
-static char *rcsId="@(#) $Id: sclguiPanel.cpp,v 1.6 2005-03-07 17:50:03 gzins Exp $"; 
+static char *rcsId="@(#) $Id: sclguiPanel.cpp,v 1.7 2005-03-08 16:33:58 gzins Exp $"; 
 static void *use_rcsId = ((void)&use_rcsId,(void *) &rcsId);
 
 /**
@@ -98,8 +102,9 @@ int main(int argc, char *argv[])
     }
 
     // Close MCS services
+    scalibPanel.Disconnect();
     mcsExit();
-    
+
     // Exit from the application with mcsSUCCESS
     exit (EXIT_SUCCESS);
 }
