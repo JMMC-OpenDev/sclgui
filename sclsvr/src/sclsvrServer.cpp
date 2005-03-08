8efcb6a5f5433cb158f9324099530acc40e14993
gzins <gzins@jmmc.fr>
2005-03-08 16:33:58 +0000

    Disconnected process from message service before exiting

--- sclsvr/src/sclsvrServer.cpp
+++ sclsvr/src/sclsvrServer.cpp
@@ -1,11 +1,14 @@
 /*******************************************************************************
  * JMMC project
  *
- * "@(#) $Id: sclsvrServer.cpp,v 1.7 2005-02-08 04:52:33 gzins Exp $"
+ * "@(#) $Id: sclsvrServer.cpp,v 1.8 2005-03-08 16:33:51 gzins Exp $"
  *
  * History
  * -------
  * $Log: not supported by cvs2svn $
+ * Revision 1.7  2005/02/08 04:52:33  gzins
+ * Printed ou error stack when connection to MCS services fails
+ *
  * Revision 1.6  2005/02/07 15:01:21  gzins
  * Added CVS log as modification history
  *
@@ -18,7 +21,7 @@
  * Search Calibrators SERVER
  */
 
-static char *rcsId="@(#) $Id: sclsvrServer.cpp,v 1.7 2005-02-08 04:52:33 gzins Exp $"; 
+static char *rcsId="@(#) $Id: sclsvrServer.cpp,v 1.8 2005-03-08 16:33:51 gzins Exp $"; 
 static void *use_rcsId = ((void)&use_rcsId,(void *) &rcsId);
 
 
@@ -69,6 +72,10 @@ int main(int argc, char *argv[])
     {
         errDisplayStack();
     }
+
+    // Close MCS services
+    scalibServer.Disconnect();
+    mcsExit();
 
     // Exit from the application with mcsSUCCESS
     exit (EXIT_SUCCESS);
