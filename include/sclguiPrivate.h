97689cd9cb2a9d8099c172d97fcb1ec0a317fe59
gzins <gzins@jmmc.fr>
2005-07-07 05:12:29 +0000

    Cleaned up

--- include/sclguiPrivate.h
+++ include/sclguiPrivate.h
@@ -3,40 +3,27 @@
 /*******************************************************************************
  * JMMC project
  *
- * "@(#) $Id: sclguiPrivate.h,v 1.2 2005-02-15 15:57:24 gzins Exp $"
+ * "@(#) $Id: sclguiPrivate.h,v 1.3 2005-07-07 05:12:29 gzins Exp $"
  *
  * History
  * -------
  * $Log: not supported by cvs2svn $
- * sccmgr    23-Nov-2004  Created
- *
  ******************************************************************************/
 
 /**
  * \file
- * Brief description of the header file, which ends at this dot.
+ * sclgui private header file.
  */
-
-/* The following piece of code alternates the linkage type to C for all 
-functions declared within the braces, which is necessary to use the 
-functions in C++-code.
-*/
-#ifdef __cplusplus
-extern "C" {
-#endif
 
 
 /*
- * Module name
+ * Constants definition
  */
+
+/* Module name */
 #define MODULE_ID "sclgui"
 
  
-
-#ifdef __cplusplus
-}
-#endif
-
 
 #endif /*!sclguiPrivate_H*/
 
