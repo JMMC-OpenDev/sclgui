324215c62ba53eec56cd1c657b79858f7312c49d
scetre <scetre@jmmc.fr>
2005-08-08 11:51:00 +0000

    add sclgui.h file

---
+++ include/sclgui.h
@@ -0,0 +1,29 @@
+#ifndef sclgui_H
+#define sclgui_H
+/*******************************************************************************
+ * JMMC project
+ *
+ * "@(#) $Id: sclgui.h,v 1.4 2005-08-08 11:51:00 scetre Exp $"
+ *
+ * History
+ * -------
+ * $Log: not supported by cvs2svn $
+ * Revision 1.2  2005/02/15 15:57:24  gzins
+ * Added CVS log as file modification history
+ *
+ * mella     25-Nov-2004  Created
+ *
+ ******************************************************************************/
+
+/**
+ * \file
+ * Summary header file for the sclgui module.
+ */
+
+/*
+ * List Cpp class declaration files.
+ */
+
+#endif /*!sclgui_H*/
+
+/*___oOo___*/
