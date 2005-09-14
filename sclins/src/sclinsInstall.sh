649da2e702b21dcde855250c9341a29d2fade3dc
gzins <gzins@jmmc.fr>
2005-09-14 22:04:06 +0000

    Added -c and -t options + improved checks

--- sclins/src/sclinsInstall.sh
+++ sclins/src/sclinsInstall.sh
@@ -2,11 +2,14 @@
 #*******************************************************************************
 # JMMC project
 #
-# "@(#) $Id: sclinsInstall.sh,v 1.5 2005-02-17 14:05:39 gzins Exp $"
+# "@(#) $Id: sclinsInstall.sh,v 1.6 2005-09-14 22:04:06 gzins Exp $"
 #
 # History
 # -------
 # $Log: not supported by cvs2svn $
+# Revision 1.5  2005/02/17 14:05:39  gzins
+# Changed 'make all man install' to 'make clean all man install'
+#
 # Revision 1.4  2005/02/15 16:02:02  gzins
 # Added CVS log as file modification history
 #
@@ -43,40 +46,93 @@
 #-------------------------------------------------------------------------------
 #
 
-# Check number of argument 
-if [ $# != 0 -a $# != 1 ]
+# Print usage 
+function printUsage () {
+        echo -e "Usage: sclinsInstall [-h] [-c] [-u] [-t tag]" 
+        echo -e "\t-h\tprint this help."
+        echo -e "\t-c\tonly compile; i.e. do not retrieve modules from "
+        echo -e "\t\trepository."
+        echo -e "\t-u\tdo not delete modules to be installed from the "
+        echo -e "\t\tcurrent directory; they are just updated."
+        echo -e "\t-t tag\tuse revision 'tag' when retrieving modules.\n"
+        exit 1;
+}
+
+# Parse command-line parameters
+update="no";
+retrieve="yes";
+tag="";
+while getopts "chut:" option
+# Initial declaration.
+# c, h, u and t are the options (flags) expected.
+# The : after option 't' shows it will have an argument passed with it.
+do
+  case $option in
+    h ) # Help option
+        printUsage ;;
+    u ) # Update option
+        update="yes";;
+    c ) # Update option
+        retrieve="no";;
+    t ) # Update option
+        tag="$OPTARG";;
+    * ) # Unknown option
+        printUsage ;;
+    esac
+done
+
+# Check that all options have been parsed 
+if [ $# -ge $OPTIND ]
 then 
     echo -e "\nUsage: sclinsInstall [-h] [-u]" 
     exit 1
 fi
 
-# Check -h option 
-update="no"
-if [ $# == 1 ]
-then  
-    if [ "$1" == "-h" ]
-    then
-        echo -e "Usage: sclinsInstall [-h] [-u]" 
-        echo -e "\t-h\tprint this help."
-        echo -e "\t-u\tdo not delete modules to be installed from the "
-        echo -e "\t\tcurrent directory; they are just updated.\n"
-        exit 1;
-    elif [ "$1" == "-u" ]
-    then
-        update="yes"
-    else
-        echo -e "\nERROR : '$1' unknown option.\n" 
-        exit 1
-    fi
+#
+# Check that the script is not run by 'root'
+if [ `whoami` == "root" ]
+then
+    echo -e "\nERROR : MCS installation MUST NOT BE done as root !!" 
+    echo -e "\n  ->  Please log in as swmgr, and start again.\n" 
+    exit 1
 fi
 
+# Check that MCSROOT is defined
+if [ "$MCSROOT" == "" ]
+then
+    echo -e "\nWARNING : MCSROOT is not defined!!"
+    echo -e ""
+    exit 1
+fi
+
+# Check that MCS configuration file is installed
+if [ ! -f $MCSROOT/etc/mcs.sh ]
+then
+    echo -e "\nWARNING : MCS configuration files not installed!!"
+    echo -e "Install mcscfg module first, and restart MCS installation!!"
+    echo -e ""
+    exit 1
+fi
+
+
+# Get intallation directory
+if [ "$INTROOT" != "" ]
+then
+    insDirName="INTROOT"
+    insDir=$INTROOT
+else
+    insDirName="MCSROOT"
+    insDir=$MCSROOT
+fi
+
+#
 # Get the current directory
 dir=$PWD
 
 # Propose the user to continue or abort
 echo -e "\n-> All the SCALIB modules will be installed (or just updated) from"
 echo -e "   '$dir' directory\n"
-if [ "$update" == "no" ]
+if [ "$update" == "no" -a  "$retrieve" == "yes" ]
 then
     echo -e "    WARNING: modules to be installed will be removed first"
     echo -e "    from the current directory. Use '-u' option to only "
@@ -86,36 +142,65 @@ echo -e "    Press enter to continue or ^C to abort "
 read choice
 
 # List of SCALIB modules
-scalib_modules="simcli alx vobs sclsvr sclgui"
-
-# Delete modules first
-cd $dir
-if [ "$update" == "no" ]
-then
-    echo -e "Deleting modules..."
-    rm -rf $scalib_modules
-fi 
+scalibModules="simcli alx vobs sclsvr sclgui"
 
 # Log file
 mkdir -p INSTALL
 logfile="$dir/INSTALL/sclinsInstall.log"
 rm -f $logfile
 
-# Retrieve modules from CVS repository
-cd $dir
-echo -e "Retrieving modules from repository..."
-cvs co $scalib_modules > $logfile 2>&1
-if [ $? != 0 ]
+# If modules have to be retrieved from repository
+if [ "$retrieve" == "yes" ]
 then
-    echo -e "\nERROR: 'cvs co $scalib_modules' failed ... \n"; 
-    tail $logfile
-    echo -e "See log file '$logfile' for details."
-    exit 1;
+    # Delete modules first
+    cd $dir
+    if [ "$update" == "no" ]
+    then
+        echo -e "Deleting modules..."
+        rm -rf $scalibModules
+    fi 
+
+    # Retrieve modules from CVS repository
+    # When a revision tag is specified, we have first to retrieve module giving
+    # this tag, and then to retrieve again to create empty directories which are
+    # not created by cvs command when '-r' option is used.
+    echo -e "Retrieving modules from repository..."
+    cd $dir
+    if [ "$tag" != "" ]
+    then
+        cvs co -r $tag $scalibModules > $logfile 2>&1
+        if [ $? != 0 ]
+        then
+            echo -e "\nERROR: 'cvs co -r $tag $scalibModules' failed ... \n"; 
+            tail $logfile
+            echo -e "See log file '$logfile' for details."
+            exit 1;
+        fi
+    fi
+
+    cvs co $scalibModules > $logfile 2>&1
+    if [ $? != 0 ]
+    then
+        echo -e "\nERROR: 'cvs co $scalibModules' failed ... \n"; 
+        tail $logfile
+        echo -e "See log file '$logfile' for details."
+        exit 1;
+    fi
 fi
+
+# Check all modules are there
+for mod in $scalibModules; do
+    cd $dir
+    if [ ! -d $mod ]
+    then
+        echo -e "\nERROR: '$mod' must be retrieved from repository first ...\n";
+        exit 1
+    fi
+done
 
 # Compile and install them
 echo -e "Building modules..."
-for mod in $scalib_modules; do
+for mod in $scalibModules; do
     cd $dir
     echo -e "    $mod..."
     cd $mod/src 
