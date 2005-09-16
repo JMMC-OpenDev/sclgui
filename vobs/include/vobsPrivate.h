a3e26ef543a3fd57ac3deb9b8b642e508ef9ea02
scetre <scetre@jmmc.fr>
2005-09-16 13:41:38 +0000

    define server address

--- vobs/include/vobsPrivate.h
+++ vobs/include/vobsPrivate.h
@@ -3,11 +3,14 @@
 /*******************************************************************************
 * JMMC project
 *
-* "@(#) $Id: vobsPrivate.h,v 1.3 2005-01-26 08:10:32 scetre Exp $"
+* "@(#) $Id: vobsPrivate.h,v 1.4 2005-09-16 13:41:38 scetre Exp $"
 *
 * History
 * -------
 * $Log: not supported by cvs2svn $
+* Revision 1.3  2005/01/26 08:10:32  scetre
+* change history
+*
 * scetre    07-Jul-2004  Created
 *
 *
@@ -18,7 +21,17 @@
 #endif
 
 #define MODULE_ID "vobs"
+/**
+ * Define server and mirrors adress
+ */
+#define vobsVIZIER_STRASBOURG_ADDRESS "vizier.u-strasbg.fr"
+#define vobsVIZIER_HAWAI_ADDRESS "www.ukirt.jach.hawaii.edu"
+#define vobsVIZIER_ENGLAND_ADDRESS "archive.ast.cam.ac.uk"
+#define vobsVIZIER_USA_ADDRESS "vizier.cfa.harvard.edu"
+#define vobsVIZIER_INDIA_ADDRESS "urania.iucaa.ernet.in"
+#define vobsVIZIER_JAPAN_ADDRESS "vizier.nao.ac.jp"
 
+#define vobsQUERY_PORT 80
 #endif /*!vobsPrivate_H*/
 
 
