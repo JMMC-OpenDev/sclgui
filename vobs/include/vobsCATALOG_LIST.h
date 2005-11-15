d62e893ecae02185459213eb742220b3be2ec183
scetre <scetre@jmmc.fr>
2005-11-15 14:56:43 +0000

    Create catalog list

---
+++ vobs/include/vobsCATALOG_LIST.h
@@ -0,0 +1,98 @@
+#ifndef vobsCATALOG_LIST_H
+#define vobsCATALOG_LIST_H
+/*******************************************************************************
+ * JMMC project
+ *
+ * "@(#) $Id: vobsCATALOG_LIST.h,v 1.1 2005-11-15 14:56:43 scetre Exp $"
+ *
+ * History
+ * -------
+ * $Log: not supported by cvs2svn $
+ ******************************************************************************/
+
+/**
+ * \file
+ * Declaration of vobsCATALOG_LIST class.
+ */
+
+#ifndef __cplusplus
+#error This is a C++ include file and cannot be used from plain C
+#endif
+
+/*
+ * System headers
+ */
+#include <map>
+
+/*
+ * MCS header
+ */
+#include "mcs.h"
+
+#include "vobsCATALOG.h"
+#include "vobsCATALOG_ASCC.h"
+#include "vobsCATALOG_BSC.h"
+#include "vobsCATALOG_CHARM.h"
+#include "vobsCATALOG_CHARM2.h"
+#include "vobsCATALOG_CIO.h"
+#include "vobsCATALOG_DENIS.h"
+#include "vobsCATALOG_DENIS_JK.h"
+#include "vobsCATALOG_HIC.h"
+#include "vobsCATALOG_LBSI.h"
+#include "vobsCATALOG_MERAND.h"
+#include "vobsCATALOG_MASS.h"
+#include "vobsCATALOG_PHOTO.h"
+#include "vobsCATALOG_SBSC.h"
+#include "vobsCATALOG_MIDI.h"
+/*
+ * Class declaration
+ */
+
+/**
+ * 
+ * 
+ */
+class vobsCATALOG_LIST
+{
+
+public:
+    // Class constructor
+    vobsCATALOG_LIST();
+
+    // Class destructor
+    virtual ~vobsCATALOG_LIST();
+
+    vobsCATALOG * Get(string catalogName);
+
+protected:
+    
+private:
+    // Declaration of copy constructor and assignment operator as private
+    // methods, in order to hide them from the users.
+    vobsCATALOG_LIST(const vobsCATALOG_LIST&);
+    vobsCATALOG_LIST& operator=(const vobsCATALOG_LIST&);
+
+    map<string, vobsCATALOG *> _catalogList;
+    map<string, vobsCATALOG *>::iterator _catalogListIterator;
+
+    // List of available catalogs
+    vobsCATALOG_CIO      _cio;
+    vobsCATALOG_PHOTO    _photo;
+    vobsCATALOG_ASCC     _ascc;
+    vobsCATALOG_HIC      _hic;
+    vobsCATALOG_MASS     _mass;
+    vobsCATALOG_LBSI     _lbsi;
+    vobsCATALOG_MERAND   _merand;
+    vobsCATALOG_CHARM    _charm;
+    vobsCATALOG_CHARM2   _charm2;
+    vobsCATALOG_BSC      _bsc;
+    vobsCATALOG_SBSC     _sbsc;
+    vobsCATALOG_DENIS    _denis;
+    vobsCATALOG_DENIS_JK _denisJK;
+    vobsCATALOG_MIDI     _midi;
+};
+
+
+#endif /*!vobsCATALOG_LIST_H*/
+
+/*___oOo___*/
---
+++ vobs/src/vobsCATALOG_LIST.cpp
@@ -0,0 +1,104 @@
+/*******************************************************************************
+ * JMMC project
+ *
+ * "@(#) $Id: vobsCATALOG_LIST.cpp,v 1.1 2005-11-15 14:56:43 scetre Exp $"
+ *
+ * History
+ * -------
+ * $Log: not supported by cvs2svn $
+ ******************************************************************************/
+
+/**
+ * \file
+ *  Definition of vobsCATALOG_LIST class.
+ */
+
+static char *rcsId="@(#) $Id: vobsCATALOG_LIST.cpp,v 1.1 2005-11-15 14:56:43 scetre Exp $"; 
+static void *use_rcsId = ((void)&use_rcsId,(void *) &rcsId);
+
+/* 
+ * System Headers 
+ */
+#include <iostream>
+using namespace std;
+
+/*
+ * MCS Headers 
+ */
+#include "mcs.h"
+#include "log.h"
+#include "err.h"
+
+/*
+ * Local Headers 
+ */
+#include "vobsCATALOG_LIST.h"
+#include "vobsPrivate.h"
+
+/**
+ * Class constructor
+ */
+vobsCATALOG_LIST::vobsCATALOG_LIST()
+{
+    // The constructor add all catalog in the map
+    _catalogList[vobsCATALOG_ASCC_ID]=&_ascc;
+    _catalogList[vobsCATALOG_BSC_ID]=&_bsc;
+    _catalogList[vobsCATALOG_CHARM2_ID]=&_charm2;
+    _catalogList[vobsCATALOG_CHARM_ID]=&_charm;
+    _catalogList[vobsCATALOG_CIO_ID]=&_cio;
+    _catalogList[vobsCATALOG_PHOTO_ID]=&_photo;
+    _catalogList[vobsCATALOG_DENIS_ID]=&_denis;
+    _catalogList[vobsCATALOG_DENIS_JK_ID]=&_denisJK;
+    _catalogList[vobsCATALOG_HIC_ID]=&_hic;
+    _catalogList[vobsCATALOG_LBSI_ID]=&_lbsi;
+    _catalogList[vobsCATALOG_MASS_ID]=&_mass;
+    _catalogList[vobsCATALOG_MERAND_ID]=&_merand;
+    _catalogList[vobsCATALOG_MIDI_ID]=&_midi;
+    _catalogList[vobsCATALOG_SBSC_ID]=&_sbsc;
+}
+
+/**
+ * Class destructor
+ */
+vobsCATALOG_LIST::~vobsCATALOG_LIST()
+{
+}
+
+/*
+ * Public methods
+ */
+
+/**
+ * Get a catalog from its name
+ *
+ * @param catalogName the name of the catalog to get
+ *
+ * @return the corresponding catalog, NULL if not found.
+ */
+vobsCATALOG *vobsCATALOG_LIST::Get(string catalogName)
+{
+    logTrace("vobsCATALOG_LIST::Get()");
+    
+    _catalogListIterator = _catalogList.find(catalogName);
+    
+    // Check if catalog is present in the list
+    if (_catalogList.find(catalogName) == _catalogList.end())
+    {
+        // errAdd
+        return NULL;
+    }
+    
+    return (_catalogListIterator->second);
+}
+
+/*
+ * Protected methods
+ */
+
+
+/*
+ * Private methods
+ */
+
+
+/*___oOo___*/
