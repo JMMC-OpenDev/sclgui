ecb7533d3d4a966536bf2af2191ba256088f2b54
scetre <scetre@jmmc.fr>
2005-11-24 15:16:14 +0000

    Used vobsBASE_FILTER instead of vobsFILTER

--- sclgui/src/sclguiMULTIPLICITY_FILTER_VIEW.cpp
+++ sclgui/src/sclguiMULTIPLICITY_FILTER_VIEW.cpp
@@ -1,11 +1,14 @@
 /*******************************************************************************
  * JMMC project
  *
- * "@(#) $Id: sclguiMULTIPLICITY_FILTER_VIEW.cpp,v 1.4 2005-10-18 12:52:48 lafrasse Exp $"
+ * "@(#) $Id: sclguiMULTIPLICITY_FILTER_VIEW.cpp,v 1.5 2005-11-24 15:16:14 scetre Exp $"
  *
  * History
  * -------
  * $Log: not supported by cvs2svn $
+ * Revision 1.4  2005/10/18 12:52:48  lafrasse
+ * First code revue
+ *
  * Revision 1.3  2005/10/11 15:24:15  scetre
  * New class of MVC second generation added. Removed Obsolete class. Changed Class present in the two versions.
  *
@@ -16,7 +19,7 @@
  * Definition of sclguiMULTIPLICITY_FILTER_VIEW class.
  */
 
-static char *rcsId="@(#) $Id: sclguiMULTIPLICITY_FILTER_VIEW.cpp,v 1.4 2005-10-18 12:52:48 lafrasse Exp $"; 
+static char *rcsId="@(#) $Id: sclguiMULTIPLICITY_FILTER_VIEW.cpp,v 1.5 2005-11-24 15:16:14 scetre Exp $"; 
 static void *use_rcsId = ((void)&use_rcsId,(void *) &rcsId);
 
 
@@ -131,7 +134,7 @@ mcsLOGICAL sclguiMULTIPLICITY_FILTER_VIEW::IsMultiplicityAuthorized()
 {
     logTrace("sclguiMULTIPLICITY_FILTER_VIEW::IsMultiplicityAuthorized()");
     
-    vobsFILTER* filter = _filterListModel->GetFilter(vobsMULTIPLICITY_FILTER_NAME);
+    vobsBASE_FILTER* filter = _filterListModel->GetFilter(vobsMULTIPLICITY_FILTER_NAME);
 
     if (filter == NULL)
     {
