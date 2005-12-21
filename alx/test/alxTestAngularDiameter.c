8a3235d9e0e74a69bce17b277a41fd564817ba78
scetre <scetre@jmmc.fr>
2005-12-21 14:59:57 +0000

    Added test with faint object

--- alx/test/alxTestAngularDiameter.c
+++ alx/test/alxTestAngularDiameter.c
@@ -1,11 +1,14 @@
 /*******************************************************************************
  * JMMC project
  *
- * "@(#) $Id: alxTestAngularDiameter.c,v 1.8 2005-12-19 20:46:49 gzins Exp $"
+ * "@(#) $Id: alxTestAngularDiameter.c,v 1.9 2005-12-21 14:59:57 scetre Exp $"
  *
  * History
  * -------
  * $Log: not supported by cvs2svn $
+ * Revision 1.8  2005/12/19 20:46:49  gzins
+ * Updated test programs according new function names
+ *
  * Revision 1.7  2005/07/06 05:07:05  gzins
  * Fixed API to alxComputeAngularDiameter
  * Added test case
@@ -38,7 +41,7 @@
  * Test program of the function which computes the angular angle of the star. 
  */
 
-static char *rcsId="@(#) $Id: alxTestAngularDiameter.c,v 1.8 2005-12-19 20:46:49 gzins Exp $"; 
+static char *rcsId="@(#) $Id: alxTestAngularDiameter.c,v 1.9 2005-12-21 14:59:57 scetre Exp $"; 
 static void *use_rcsId = ((void)&use_rcsId,(void *) &rcsId);
 
 
@@ -70,7 +73,7 @@ static void *use_rcsId = ((void)&use_rcsId,(void *) &rcsId);
 int main (int argc, char *argv[])
 {
     /* Configure logging service */
-    logSetStdoutLogLevel(logTEST);
+    logSetStdoutLogLevel(logDEBUG);
     logSetPrintDate(mcsFALSE);
     logSetPrintFileLine(mcsFALSE);
 
@@ -104,6 +107,22 @@ int main (int argc, char *argv[])
     mgK.value = 2.636;  
     if (alxComputeAngularDiameterForBrightStar
         (mgB, mgV, mgR, mgK, &diameters)== mcsFAILURE)
+    {
+        return mcsFAILURE;
+    }
+
+    alxDATA mgI;
+    alxDATA mgJ;
+    alxDATA mgKf;
+    alxDATA mgH;
+    mgI.value = 11.250; mgI.confIndex = alxCONFIDENCE_HIGH; mgI.isSet = mcsTRUE;
+    mgJ.value = 9.062; mgJ.confIndex = alxCONFIDENCE_HIGH; mgJ.isSet = mcsTRUE;
+    mgKf.value = 8.143;  mgKf.confIndex = alxCONFIDENCE_HIGH; mgKf.isSet = mcsTRUE;
+    mgH.value = 8.305;  mgH.confIndex = alxCONFIDENCE_HIGH; mgH.isSet = mcsTRUE;
+    
+    alxDIAMETERS diametersFaint;
+    if (alxComputeAngularDiameterForFaintStar
+        (mgI, mgJ, mgKf, mgH, &diametersFaint)== mcsFAILURE)
     {
         return mcsFAILURE;
     }
