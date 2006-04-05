9ae83ff074b5a65514316bdbb69e5e5b4dc88c46
yvander <yvander@jmmc.fr>
2006-04-05 12:41:23 +0000

    Voir commentaire dans le fichier

--- sclgui/src/fr/jmmc/scalib/sclgui/MultiplicityFilter.java
+++ sclgui/src/fr/jmmc/scalib/sclgui/MultiplicityFilter.java
@@ -1,18 +1,29 @@
 /*******************************************************************************
  * JMMC project
  *
- * "@(#) $Id: MultiplicityFilter.java,v 1.1 2006-03-27 11:59:58 lafrasse Exp $"
+ * "@(#) $Id: MultiplicityFilter.java,v 1.2 2006-04-05 12:41:23 yvander Exp $"
  *
  * History
  * -------
  * $Log: not supported by cvs2svn $
+ * Revision 1.1  2006/03/27 11:59:58  lafrasse
+ * Added new experimental Java GUI
+ *
  ******************************************************************************/
 package jmmc.scalib.sclgui;
 
 import jmmc.mcs.log.MCSLogger;
 
 import java.util.Vector;
-
+/*******
+*
+*
+* VOIR AVEC GUILLAUME !
+* On ne prend pas en compte la checkbox multiple puisque si l'on selection multiplicity c'est que l'on interdit la multiplicite OU autorise(si autorise alors inverser le boolean).
+*
+*
+*
+*/
 
 /**
  * Multiplicity filter.
@@ -67,16 +78,43 @@ public class MultiplicityFilter extends Filter
 
         // If the filter is enabled
         if (isEnabled() == true)
+	//if ((isEnabled() == true) && (getMultiplicity() == true))
         {
-            // Get the id of the column contaning 'distance' star property
-            //int distanceId = starList.getColumnIdByName("mult???dist");
+            // Get the id of the column contaning 'multiplicity' star property
+            int multiplicityId = starList.getColumnIdByName("MultFlag");
 
             // For each row of the star list
             int rowId = 0;
 
             while (rowId < starList.size())
             {
-                rowId++;
+	        // Get the multiplicity flag value
+		Vector row = ((Vector) starList.elementAt(rowId));
+		StarProperty cell = ((StarProperty) row.elementAt(multiplicityId));
+		String multiplicityFlag = cell.toString();
+
+		// if the multiplicity flag exist
+		if ((multiplicityFlag != null) && (multiplicityFlag.equals("") == false))
+		{
+		    // if multiplicity is checked
+		    /*if (getMultiplicity() == true)
+		    {
+		        // Remove this row from the star List
+		*/	starList.remove(rowId);
+		  /*  }
+		    else
+		    {
+		        // Otherwise process the next row
+			rowwId++;
+		    }*/
+
+		}
+		else
+		{
+		    // Otherwise process the next row
+		    rowId++;
+		}
+
             }
         }
     }
