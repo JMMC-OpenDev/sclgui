3717514cb82f9ae9250ce8404646fe896250f3e3
gzins <gzins@jmmc.fr>
2006-04-05 15:08:28 +0000

    Added GetList() and GetNbCalibrators() Renamed Reset methods to Clear Deleted obsolete methods

--- sclgui/include/sclguiCALIBRATOR_LIST_MODEL.h
+++ sclgui/include/sclguiCALIBRATOR_LIST_MODEL.h
@@ -3,11 +3,14 @@
 /*******************************************************************************
  * JMMC project
  *
- * "@(#) $Id: sclguiCALIBRATOR_LIST_MODEL.h,v 1.2 2005-10-18 12:52:48 lafrasse Exp $"
+ * "@(#) $Id: sclguiCALIBRATOR_LIST_MODEL.h,v 1.3 2006-04-05 15:08:09 gzins Exp $"
  *
  * History
  * -------
  * $Log: not supported by cvs2svn $
+ * Revision 1.2  2005/10/18 12:52:48  lafrasse
+ * First code revue
+ *
  * Revision 1.1  2005/10/11 15:24:15  scetre
  * New class of MVC second generation added. Removed Obsolete class. Changed Class present in the two versions.
  *
@@ -44,6 +47,17 @@
 #include "sclguiFILTER_LIST_MODEL.h"
 
 /*
+ * Enumerate
+ */
+typedef enum
+{
+    sclguiALL_CALIBRATORS,
+    sclguiDELETED_CALIBRATORS,
+    sclguiFILTERED_CALIBRATORS,
+    sclguiCALIBRATORS_WITH_COHERENT_DIAM,
+    sclguiCALIBRATORS_WITHOUT_VAR_MULT    
+} sclguiTYPE_OF_CALIBRATORS;
+/*
  * Class declaration
  */
 
@@ -62,23 +76,23 @@ public:
     // Class destructor
     virtual ~sclguiCALIBRATOR_LIST_MODEL();
 
+    // Method related to calibrator list
     virtual mcsCOMPL_STAT SetList(msgMESSAGE &msg);
     virtual mcsCOMPL_STAT SetList(sclsvrCALIBRATOR_LIST &list);
-    virtual sclsvrCALIBRATOR_LIST *GetCalibratorList(void);
-    virtual mcsCOMPL_STAT SetFilterList(sclguiFILTER_LIST_MODEL * filterList);
-    
-    virtual mcsCOMPL_STAT DeleteCalibrator(mcsINT32      unwantedStarNumber,
-                                           miscoDYN_BUF &DeletedStarNameMsg);
-    virtual mcsCOMPL_STAT ResetCalibrators(void);
-    virtual mcsCOMPL_STAT ResetDeletedCalibrators(void);
-
-    virtual mcsFLOAT GetNbCDSReturn(void);
-    virtual mcsFLOAT GetNbCoherentDiamFound(void);
-    virtual mcsFLOAT GetNbWithoutVarMult(void);
-    virtual mcsFLOAT GetNbInFilteredList(void);
-
     virtual mcsCOMPL_STAT LoadFromFile(mcsSTRING256  fileName,
                                        sclsvrREQUEST &request);
+    virtual mcsCOMPL_STAT Clear(void);
+
+    virtual sclsvrCALIBRATOR_LIST *GetList(sclguiTYPE_OF_CALIBRATORS type);
+    virtual mcsINT32 GetNbCalibrators(sclguiTYPE_OF_CALIBRATORS type);
+
+    // Filters
+    virtual mcsCOMPL_STAT SetFilterList(sclguiFILTER_LIST_MODEL * filterList);
+    
+    // Method related to deleted calibrators
+    virtual mcsCOMPL_STAT DeleteCalibrator(mcsINT32 starNum, char *starId);
+    virtual mcsCOMPL_STAT ClearDeletedCalibratorList(void);
+
 protected:
     
 private:
