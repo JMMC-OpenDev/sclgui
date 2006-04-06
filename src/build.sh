29941e7f4f1bc39621959d1792a4dafbbf26e77c
yvander <yvander@jmmc.fr>
2006-04-06 13:04:40 +0000

    Add Aladin jar in classpath

--- src/build.sh
+++ src/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export CLASSPATH=../lib/kxml2-min.jar:../lib/cds.savot.writer.jar:../lib/cds.savot.sax.jar:../lib/cds.savot.pull.jar:../lib/cds.savot.model.jar:../lib/cds.savot.common.jar:.
+export CLASSPATH=../lib/kxml2-min.jar:../lib/cds.savot.writer.jar:../lib/cds.savot.sax.jar:../lib/cds.savot.pull.jar:../lib/cds.savot.model.jar:../lib/cds.savot.common.jar:.:../lib/Aladin.jar
 
 
 compile(){
