cba4b138049b8ef7f9e02986e5a010eb7fff7753
gmella <guillaume.mella@univ-grenoble-alpes.fr>
2006-04-07 13:07:09 +0000

    jolyzation

--- src/fr/jmmc/scalib/sclgui/VOInteraction.java
+++ src/fr/jmmc/scalib/sclgui/VOInteraction.java
@@ -1,16 +1,24 @@
 /*******************************************************************************
  * JMMC project
  *
- * "@(#) $Id: VOInteraction.java,v 1.1 2006-04-06 13:09:40 yvander Exp $"
+ * "@(#) $Id: VOInteraction.java,v 1.2 2006-04-07 13:07:09 mella Exp $"
  *
  * History
  * -------
  * $Log: not supported by cvs2svn $
+ * Revision 1.1  2006/04/06 13:09:40  yvander
+ * Aladin interaction class
+ *
  * Revision 1.1  2006/03/27 11:59:58  lafrasse
  * Added new experimental Java GUI
  *
  ******************************************************************************/
 package jmmc.scalib.sclgui;
+
+import cds.aladin.*;
+
+import cds.tools.VOApp;
+import cds.tools.VOObserver;
 
 import jmmc.mcs.log.MCSLogger;
 
@@ -28,32 +36,36 @@ import javax.swing.border.*;
 import javax.swing.event.*;
 import javax.swing.table.*;
 
-import cds.aladin.*;
-import cds.tools.VOObserver;
-import cds.tools.VOApp;
 
-public class VOInteraction implements VOApp {
-
+/**
+ * DOCUMENT ME!
+ *
+ * @author $author$
+ * @version $Revision: 1.2 $
+  */
+public class VOInteraction implements VOApp
+{
     /** aladin Software */
     VOApp _aladin;
 
     /**
      * Constructor.
      */
-    public VOInteraction(){
-
+    public VOInteraction()
+    {
     }
 
     /**
      * Launch aladin and put votable data.
      * @param votable The votable data loaded
      */
-    public void startAladin(String votable) {
-
+    public void startAladin(String votable)
+    {
         // Instantiate Aladin
         _aladin = Aladin.launch();
 
-        try {
+        try
+        {
             // Ask Aladin to load a VOTable file (you pass your own object reference
             // for possible callbacks. You can use the simpler method putVOTable(in,"MyVOTable")
             // if you do not want to be called back for showing or selecting objects
@@ -62,9 +74,11 @@ public class VOInteraction implements VOApp {
             // Prepare stream
             InputStream in = (InputStream) (new StringBufferInputStream(votable));
 
-	    // Put data in Aladin
-	    _aladin.putVOTable(this,in,"SearchCal Results");
-        } catch (Exception e) {
+            // Put data in Aladin
+            _aladin.putVOTable(this, in, "SearchCal Results");
+        }
+        catch (Exception e)
+        {
             e.printStackTrace();
         }
     }
@@ -72,81 +86,89 @@ public class VOInteraction implements VOApp {
     /**
      * When Aladin call putVOTable method
      */
-    public String putVOTable(VOApp vOApp, InputStream inputStream,
-                             String string) {
+    public String putVOTable(VOApp vOApp, InputStream inputStream, String string)
+    {
         return null;
     }
 
     /**
      * When Aladin call putVOTable method
      */
-    public String putVOTable(InputStream in, String label) {
+    public String putVOTable(InputStream in, String label)
+    {
         return null;
     }
 
     /**
      * When Aladin call getVOTable method
      */
-    public InputStream getVOTable(String dataID) {
+    public InputStream getVOTable(String dataID)
+    {
         return null;
     }
 
     /**
      * When Aladin call putFits method
      */
-    public String putFITS(InputStream in, String label) {
+    public String putFITS(InputStream in, String label)
+    {
         return null;
     }
 
     /**
      * When Aladin call getFits method
      */
-    public InputStream getFITS(String dataID) {
+    public InputStream getFITS(String dataID)
+    {
         return null;
     }
 
     /**
      * When Aladin call showVOTableObject method
      */
-    public void showVOTableObject(String oid[]) {
+    public void showVOTableObject(String[] oid)
+    {
         /*
-         System.out.print("I have to show:");
-         for (int i = 0; i < oid.length; i++) System.out.print(" " + oid[i]);
-                 System.out.println();
+           System.out.print("I have to show:");
+           for (int i = 0; i < oid.length; i++) System.out.print(" " + oid[i]);
+                   System.out.println();
          */
     }
 
     /**
      * When Aladin call selectVOTableObject method
      */
-    public void selectVOTableObject(String oid[]) {
+    public void selectVOTableObject(String[] oid)
+    {
         /*System.out.print("I have to select:");
-
-        for (int i = 0; i < oid.length; i++) {
-            System.out.print(" " + oid[i]);
-
-            //this.panelres.tableresult.addRowSelectionInterval(this.panelres.sorter.viewIndex(Integer.parseInt(oid[i])),				                                                                   this.panelres.sorter.viewIndex(Integer.parseInt(oid[i])));
-        }
-        System.out.println();
-*/
+           for (int i = 0; i < oid.length; i++) {
+               System.out.print(" " + oid[i]);
+               //this.panelres.tableresult.addRowSelectionInterval(this.panelres.sorter.viewIndex(Integer.parseInt(oid[i])),                                                                                                   this.panelres.sorter.viewIndex(Integer.parseInt(oid[i])));
+           }
+           System.out.println();
+         */
     }
 
     /**
      * When Aladin call setVisible method
      */
-    public void setVisible(boolean flag) {}
+    public void setVisible(boolean flag)
+    {
+    }
 
     /**
      * When Aladin call execCommand method
      */
-    public String execCommand(String cmd) {
+    public String execCommand(String cmd)
+    {
         return null;
     }
 
     /**
      * When Aladin call addObserve method
      */
-    public void addObserver(VOObserver vOObserver, int _int) {
+    public void addObserver(VOObserver vOObserver, int _int)
+    {
     }
 }
 /*___oOo___*/
