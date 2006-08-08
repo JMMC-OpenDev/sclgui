ae038d6d54f5bd24637a7f895c9ec3f58a839756
sly74fr <slymacdev@free.fr>
2006-08-08 16:14:23 +0000

    Updated to properly handle widget order Streamlined filter processing code

--- src/fr/jmmc/scalib/sclgui/LuminosityFilter.java
+++ src/fr/jmmc/scalib/sclgui/LuminosityFilter.java
@@ -1,11 +1,14 @@
 /*******************************************************************************
  * JMMC project
  *
- * "@(#) $Id: LuminosityFilter.java,v 1.4 2006-08-04 14:09:10 lafrasse Exp $"
+ * "@(#) $Id: LuminosityFilter.java,v 1.5 2006-08-08 16:14:23 lafrasse Exp $"
  *
  * History
  * -------
  * $Log: not supported by cvs2svn $
+ * Revision 1.4  2006/08/04 14:09:10  lafrasse
+ * Added GUI enabling/disabling feature to filters
+ *
  * Revision 1.3  2006/04/06 14:38:58  lafrasse
  * Layout update
  *
@@ -23,16 +26,18 @@ package jmmc.scalib.sclgui;
 
 import jmmc.mcs.log.MCSLogger;
 
-import java.util.Enumeration;
-import java.util.Vector;
+import java.util.*;
 
 
 /**
  *  LuminosityFilter filter.
  */
-public class LuminosityFilter extends Filter
+public class LuminosityFilter extends Filter implements Observer
 {
     //TODO place a constant for every "luminosity" ;))
+
+    /** String containing all the luminosty class names to be rejected */
+    String _rejectedLuminosityClasses = new String();
 
     /**
      * Default constructor.
@@ -41,12 +46,14 @@ public class LuminosityFilter extends Filter
     {
         super();
 
-        _constraints.put("I", new Boolean(false));
-        _constraints.put("II", new Boolean(false));
-        _constraints.put("III", new Boolean(false));
-        _constraints.put("IV", new Boolean(false));
-        _constraints.put("V", new Boolean(false));
-        _constraints.put("VI", new Boolean(false));
+        addObserver(this);
+
+        setConstraint("I", new Boolean(false));
+        setConstraint("II", new Boolean(false));
+        setConstraint("III", new Boolean(false));
+        setConstraint("IV", new Boolean(false));
+        setConstraint("V", new Boolean(false));
+        setConstraint("VI", new Boolean(false));
     }
 
     /**
@@ -62,36 +69,28 @@ public class LuminosityFilter extends Filter
     }
 
     /**
-     * Return the luminosity classes selected.
-     *
-     * @return lumClass a vector of luminosity class.
+     * @sa java.util.Observer
      */
-    private Vector getAllowedLuminosity()
+    public void update(Observable o, Object arg)
     {
         MCSLogger.trace();
 
-        Vector lumClass = new Vector();
-
-        // Checkboxes values with elements (true or false) and keys (indexes)
-        Enumeration elements = _constraints.elements();
-        Enumeration keys     = _constraints.keys();
-
-        while (elements.hasMoreElements())
+        // For each spectral type constraint
+        for (int i = 0; i < getNbOfConstraints(); i++)
         {
-            // Next data
-            boolean bool          = ((Boolean) elements.nextElement()).booleanValue();
-            String  lumClassValue = (String) keys.nextElement();
+            // Get the spectral type name
+            String luminosityClassName = getConstraintNameByOrder(i);
 
-            // Checkbox selected
-            if (bool == true)
+            // Get the spectral type state (checked or not)
+            boolean luminosityClassState = ((Boolean) getConstraintByName(luminosityClassName)).booleanValue();
+
+            // If the spectral type must be rejected (is checked)
+            if (luminosityClassState == true)
             {
-                // Add in Vector
-                lumClass.add(lumClassValue);
+                // Add the its name in the rejected spectral type string
+                _rejectedLuminosityClasses += luminosityClassName;
             }
         }
-
-        // Return a vector
-        return lumClass;
     }
 
     /**
@@ -106,124 +105,7 @@ public class LuminosityFilter extends Filter
         // If the filter is enabled
         if ((isEnabled() == true))
         {
-            // Get the id of the column contaning 'SpType' star property
-            int spectralTypeId = starList.getColumnIdByName("SpType");
-
-            // For each row of the star list
-            int rowId = 0;
-
-            while (rowId < starList.size())
-            {
-                // Get the spectral type value of the star
-                Vector       row               = ((Vector) starList.elementAt(rowId));
-                StarProperty cell              = ((StarProperty) row.elementAt(spectralTypeId));
-                String       spectralTypeValue = cell.toString();
-
-                // To determine if the end of luminosity class in spectral type
-                boolean endOfLum = false;
-
-                // The spectral type exist
-                if ((spectralTypeValue != null) &&
-                        (spectralTypeValue.equals("") == false))
-                {
-                    // Meter of character
-                    int beginIdx = 0;
-                    int endIdx   = 0;
-
-                    // Loop until the beginning of luminosity class
-                    while ((beginIdx < spectralTypeValue.length()) &&
-                            (spectralTypeValue.charAt(beginIdx) != 'I') &&
-                            (spectralTypeValue.charAt(beginIdx) != 'V'))
-                    {
-                        beginIdx++;
-                    }
-
-                    //endIdx >= beginIdx
-                    endIdx = beginIdx;
-
-                    // Loop until the end of luminosity class
-                    while ((endIdx < spectralTypeValue.length()) &&
-                            (endOfLum != true))
-                    {
-                        //
-                        if ((spectralTypeValue.charAt(endIdx) == 'I') ||
-                                (spectralTypeValue.charAt(endIdx) == 'V'))
-                        {
-                            //
-                            endIdx++;
-                        }
-                        else
-                        {
-                            //
-                            endOfLum = true;
-                        }
-                    }
-
-                    // Get the luminosity classes selected
-                    Vector lumClassSelected = getAllowedLuminosity();
-
-                    // If luminosity classes are checked
-                    if (lumClassSelected.size() > 0)
-                    {
-                        // Find or not a luminosity class corresponding to the selection
-                        boolean find = false;
-
-                        // For each luminosity class checked
-                        for (int i = 0; i < lumClassSelected.size(); i++)
-                        {
-                            // The value of spectral type checked
-                            String valueSelected  = (String) lumClassSelected.elementAt(i);
-                            String starLuminosity = spectralTypeValue.substring(beginIdx,
-                                    endIdx);
-
-                            // Luminosity class are identicals
-                            if (starLuminosity.equals(valueSelected) == true)
-                            {
-                                // Do not remove
-                                find = true;
-                            }
-                        }
-
-                        // Data Not found
-                        if (find == false)
-                        {
-                            // Remove star to the starList
-                            starList.remove(rowId);
-                        }
-                        else
-                        {
-                            // Otherwise process the next row
-                            rowId++;
-                        }
-                    }
-                    else
-                    {
-                        // There is not luminosity class in spectral type
-                        if (beginIdx == spectralTypeValue.length())
-                        {
-                            // Remove star to the starList
-                            starList.remove(rowId);
-                        }
-                        else
-                        {
-                            // Otherwise process the next row
-                            rowId++;
-                        }
-                    }
-                }
-
-                // The spectral type does not exist
-                else
-                {
-                    // Remove star to the starList
-                    starList.remove(rowId);
-
-                    /* OR
-                       // Otherwise process the next row
-                       rowId++;
-                     */
-                }
-            }
+            // TODO : implement (see SpectralTypeFilter.java for an example)
         }
     }
 }
