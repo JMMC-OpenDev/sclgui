bce004b9bb4544cfc4bb4a2c57632af80e1b1293
gzins <gzins@jmmc.fr>
2006-08-25 07:03:10 +0000

    New name for tests related to 'bright' scenario

---
+++ sclsvr/test/sclsvrTestBright_Tau04Eri.sh
@@ -0,0 +1,15 @@
+#!/bin/bash 
+#*******************************************************************************
+# JMMC project
+#
+# "@(#) $Id: sclsvrTestBright_Tau04Eri.sh,v 1.1 2006-08-25 06:58:56 gzins Exp $"
+#
+# History
+# -------
+# $Log: not supported by cvs2svn $
+# Revision 1.1  2006/04/10 15:20:56  gzins
+# Added
+#
+#*******************************************************************************
+
+sclsvrServer -v 3 -noDate -noFileLine GETCAL "-objectName tau04_Eri -mag -1.17 -diffRa 3600 -diffDec 1200 -band K -minMagRange -1 -maxMagRange 2 -ra 03:19:31.002 -dec -21:45:28.310 -baseMax 102.45 -wlen 2.2"
