0bdbc93f991bd080371074e3f85e97806a74e011
JMMC-OpenDev <jmmc-tech-group@jmmc.fr>
2006-10-31 09:56:25 +0000

    This commit was manufactured by cvs2svn to create tag 'v0_0/sclcat'.

---
+++ sclcat/include/sclcatPrivate.h
@@ -0,0 +1,30 @@
+#ifndef sclcatPrivate_H
+#define sclcatPrivate_H
+/*******************************************************************************
+ * JMMC project
+ *
+ * "@(#) $Id: sclcatPrivate.h,v 1.1.1.1 2006-10-31 09:56:24 sccmgr Exp $"
+ *
+ * History
+ * -------
+ * $Log: not supported by cvs2svn $
+ ******************************************************************************/
+
+/**
+ * @file
+ * sclcat private header file.
+ */
+
+
+/*
+ * Constants definition
+ */
+
+/* Module name */
+#define MODULE_ID "sclcat"
+
+ 
+
+#endif /*!sclcatPrivate_H*/
+
+/*___oOo___*/
