11b375cec006e4654222d5a76bfb485aa001a5ea
JMMC-OpenDev <jmmc-tech-group@jmmc.fr>
2006-10-31 09:56:24 +0000

    Creation

---
+++ sclcat/include/sclcat.h
@@ -0,0 +1,27 @@
+#ifndef sclcat_H
+#define sclcat_H
+/*******************************************************************************
+ * JMMC project
+ *
+ * "@(#) $Id: sclcat.h,v 1.1.1.1 2006-10-31 09:56:24 sccmgr Exp $"
+ *
+ * History
+ * -------
+ * $Log: not supported by cvs2svn $
+ ******************************************************************************/
+
+/**
+ * @file
+ * sclcat general header file.
+ */
+
+
+/*
+ * Local headers
+ */
+
+ 
+
+#endif /*!sclcat_H*/
+
+/*___oOo___*/
