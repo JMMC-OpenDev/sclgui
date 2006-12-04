2e38ac59347e30e1bc9b660131ff2c77c225cc25
sly74fr <slymacdev@free.fr>
2006-12-04 12:35:49 +0000

    Enhanced delete menu item enabling, now based on JTable row selection detection.

--- src/fr/jmmc/scalib/sclgui/CalibratorsView.java
+++ src/fr/jmmc/scalib/sclgui/CalibratorsView.java
@@ -1,11 +1,14 @@
 /*******************************************************************************
  * JMMC project
  *
- * "@(#) $Id: CalibratorsView.java,v 1.30 2006-11-30 23:03:53 lafrasse Exp $"
+ * "@(#) $Id: CalibratorsView.java,v 1.31 2006-12-04 12:35:49 lafrasse Exp $"
  *
  * History
  * -------
  * $Log: not supported by cvs2svn $
+ * Revision 1.30  2006/11/30 23:03:53  lafrasse
+ * Added a fixed column on the left with generated star IDs.
+ *
  * Revision 1.29  2006/11/30 14:54:27  lafrasse
  * Added support for hidden legend view on startup.
  *
@@ -99,6 +102,8 @@
  ******************************************************************************/
 package jmmc.scalib.sclgui;
 
+import jmmc.mcs.gui.StatusBar;
+
 import jmmc.mcs.log.MCSLogger;
 
 import jmmc.mcs.util.*;
@@ -130,7 +135,7 @@ import javax.swing.table.*;
  * application preferences change.
  */
 public class CalibratorsView extends JPanel implements TableModelListener,
-    Observer, Printable
+    ListSelectionListener, Observer, Printable
 {
     /** Show Legend action */
     static ShowLegendAction _showLegendAction;
@@ -193,6 +198,9 @@ public class CalibratorsView extends JPanel implements TableModelListener,
         // Table initialization
         _calibratorsTable = new JTable();
 
+        // Handle the selection listener
+        _calibratorsTable.getSelectionModel().addListSelectionListener(this);
+
         TableSorter tableSorter = new TableSorter(_calibratorsModel);
         tableSorter.setTableHeader(_calibratorsTable.getTableHeader());
         _calibratorsTable.setModel(tableSorter);
@@ -242,40 +250,16 @@ public class CalibratorsView extends JPanel implements TableModelListener,
     }
 
     /**
-     * Open the given file and display it. In fact it tells to the inner model to parse it.
+     * Called on the JTable row selection changes.
      *
-     * @param file the file to display.
+     * @sa javax.swing.event.ListSelectionListener
      */
-    public void openFile(File file)
-    {
-        try
-        {
-            // Get a BufferedReader from file
-            String         fileName   = file.getAbsolutePath();
-            FileReader     fileReader = new FileReader(fileName);
-            BufferedReader in         = new BufferedReader(fileReader);
-
-            // Build CalibratorModel and parse votable
-            _calibratorsModel.parseVOTable(in);
-
-            // Finally affect this model to the main view
-        }
-        catch (Exception e)
-        {
-            // TODO handle this exception
-            e.printStackTrace();
-        }
-    }
-
-    /**
-     * Called on _calibratorsModel changes.
-     */
-    public void tableChanged(TableModelEvent e)
+    public void valueChanged(ListSelectionEvent e)
     {
         MCSLogger.trace();
 
-        // If there is any row in the table
-        if (_calibratorsTable.getRowCount() > 0)
+        // If there is any row selected in the table
+        if (_calibratorsTable.getSelectedRowCount() > 0)
         {
             // Enable the delte menu item
             _deleteAction.setEnabled(true);
@@ -285,6 +269,14 @@ public class CalibratorsView extends JPanel implements TableModelListener,
             // Disable the delte menu item
             _deleteAction.setEnabled(false);
         }
+    }
+
+    /**
+     * Called on _calibratorsModel changes.
+     */
+    public void tableChanged(TableModelEvent e)
+    {
+        MCSLogger.trace();
 
         // Enable/disable the Undelete menu item
         _undeleteAction.setEnabled(_calibratorsModel.hasSomeDeletedStars());
@@ -326,6 +318,63 @@ public class CalibratorsView extends JPanel implements TableModelListener,
     }
 
     /**
+     * @sa java.awt.print
+     */
+    public int print(Graphics graphics, PageFormat pageFormat, int pageIndex)
+        throws PrinterException
+    {
+        Graphics2D g2d = (Graphics2D) graphics;
+        g2d.translate(pageFormat.getImageableX(), pageFormat.getImageableY());
+
+        int fontHeight  = g2d.getFontMetrics().getHeight();
+        int fontDescent = g2d.getFontMetrics().getDescent();
+
+        // laisser de l'espace pour le numero de page
+        double pageHeight = pageFormat.getImageableHeight() - fontHeight;
+        double pageWidth  = pageFormat.getImageableWidth();
+
+        g2d.drawString("Page: " + (pageIndex + 1), ((int) pageWidth / 2) - 35,
+            (int) ((pageHeight + fontHeight) - fontDescent));
+
+        g2d.scale(0.7, 0.7);
+        paint(g2d);
+
+        return (Printable.PAGE_EXISTS);
+    }
+
+    /**
+     * Open the given file and display it. In fact it tells to the inner model to parse it.
+     *
+     * @param file the file to display.
+     */
+    public void openFile(File file)
+    {
+        try
+        {
+            // Get a BufferedReader from file
+            String         fileName   = file.getAbsolutePath();
+            FileReader     fileReader = new FileReader(fileName);
+            BufferedReader in         = new BufferedReader(fileReader);
+
+            StatusBar.show("loading file...");
+            System.out.println("loading file...");
+
+            // Build CalibratorModel and parse votable
+            _calibratorsModel.parseVOTable(in);
+
+            StatusBar.show("file succesfully loaded.");
+            System.out.println("file succesfully loaded.");
+        }
+        catch (Exception e)
+        {
+            StatusBar.show("an error occured while trying to load file !");
+
+            // TODO handle this exception
+            e.printStackTrace();
+        }
+    }
+
+    /**
      * Tell GUI to show or hide legend panel
      */
     public void showLegend(boolean visible)
@@ -356,31 +405,6 @@ public class CalibratorsView extends JPanel implements TableModelListener,
         MCSLogger.trace();
 
         //@todo implement
-    }
-
-    /**
-     * @sa java.awt.print
-     */
-    public int print(Graphics graphics, PageFormat pageFormat, int pageIndex)
-        throws PrinterException
-    {
-        Graphics2D g2d = (Graphics2D) graphics;
-        g2d.translate(pageFormat.getImageableX(), pageFormat.getImageableY());
-
-        int fontHeight  = g2d.getFontMetrics().getHeight();
-        int fontDescent = g2d.getFontMetrics().getDescent();
-
-        // laisser de l'espace pour le numero de page
-        double pageHeight = pageFormat.getImageableHeight() - fontHeight;
-        double pageWidth  = pageFormat.getImageableWidth();
-
-        g2d.drawString("Page: " + (pageIndex + 1), ((int) pageWidth / 2) - 35,
-            (int) ((pageHeight + fontHeight) - fontDescent));
-
-        g2d.scale(0.7, 0.7);
-        paint(g2d);
-
-        return (Printable.PAGE_EXISTS);
     }
 
     protected class DeleteAction extends MCSAction
