8f94f02fb80d4112a680c66c699f97fd8d4e2822
JMMC-OpenDev <jmmc-tech-group@jmmc.fr>
2006-12-21 15:19:19 +0000

    Fix directory structure and add additional files

---
+++ sclws/include/sclws.h
@@ -0,0 +1,27 @@
+#ifndef sclws_H
+#define sclws_H
+/*******************************************************************************
+ * JMMC project
+ *
+ * "@(#) $Id: sclws.h,v 1.1 2006-12-21 15:19:18 ccmgr Exp $"
+ *
+ * History
+ * -------
+ * $Log: not supported by cvs2svn $
+ ******************************************************************************/
+
+/**
+ * @file
+ * sclws general header file.
+ */
+
+
+/*
+ * Local headers
+ */
+
+ 
+
+#endif /*!sclws_H*/
+
+/*___oOo___*/
