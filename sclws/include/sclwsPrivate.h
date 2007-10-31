e8ccd17b2f2e3498a117db0ad3d7d89a3f7a7ebc
gzins <gzins@jmmc.fr>
2007-10-31 11:44:22 +0000

    Added sclwsReturnSoapError macro

--- sclws/include/sclwsPrivate.h
+++ sclws/include/sclwsPrivate.h
@@ -3,18 +3,20 @@
 /*******************************************************************************
  * JMMC project
  *
- * "@(#) $Id: sclwsPrivate.h,v 1.1 2006-12-21 15:19:19 ccmgr Exp $"
+ * "@(#) $Id: sclwsPrivate.h,v 1.2 2007-10-31 11:44:22 gzins Exp $"
  *
  * History
  * -------
  * $Log: not supported by cvs2svn $
+ * Revision 1.1  2006/12/21 15:19:19  ccmgr
+ * Fix directory structure and add additional files
+ *
  ******************************************************************************/
 
 /**
  * @file
  * sclws private header file.
  */
-
 
 /*
  * Constants definition
@@ -23,7 +25,14 @@
 /* Module name */
 #define MODULE_ID "sclws"
 
- 
+/** Usefull macro to return error when executing SOAP service. The error message
+ * is get from error stack */
+#define sclwsReturnSoapError()                                       \
+    soap_fault(p_soap);                                              \
+    strncpy(sclwsSoapErrMsg, errUserGet(), sizeof(sclwsSoapErrMsg)); \
+    p_soap->fault->faultstring = sclwsSoapErrMsg;                    \
+    errCloseStack();                                                 \
+    return SOAP_ERR;
 
 #endif /*!sclwsPrivate_H*/
 
