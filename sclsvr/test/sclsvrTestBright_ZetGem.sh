7e4bc465efc9f4353d1e0fa169cfe8bf0ad99717
JMMC-OpenDev <jmmc-tech-group@jmmc.fr>
2007-11-15 05:12:33 +0000

    This commit was manufactured by cvs2svn to create tag 'V3-3-7/sclsvr'.

---
+++ sclsvr/test/sclsvrTestBright_ZetGem.sh
@@ -0,0 +1,15 @@
+#!/bin/bash 
+#*******************************************************************************
+# JMMC project
+#
+# "@(#) $Id: sclsvrTestBright_ZetGem.sh,v 1.1 2006-08-25 06:58:56 gzins Exp $"
+#
+# History
+# -------
+# $Log: not supported by cvs2svn $
+# Revision 1.1  2006/04/10 15:20:56  gzins
+# Added
+#
+#*******************************************************************************
+
+sclsvrServer -v 3 -noDate -noFileLine GETCAL "-objectName zet_Gem -mag 2.12 -diffRa 1800 -diffDec 600 -band K -minMagRange 1 -maxMagRange 3 -ra 07:04:06.5318 -dec 20:34:13.069 -baseMax 102.45 -wlen 2.2"
