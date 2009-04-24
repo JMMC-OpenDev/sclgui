0b486aafea49de25b76cdfda253d44dfaa2ec7d9
JMMC-OpenDev <jmmc-tech-group@jmmc.fr>
2009-04-24 12:16:47 +0000

    This commit was manufactured by cvs2svn to create tag 'V3-4-0/sclsvr'.

---
+++ sclsvr/test/ref/AzV_266.cmd
@@ -0,0 +1,1 @@
+sclsvrServer -noDate -noFileLine -v 3 GETCAL "-minMagRange 10 -mag 12.9 -objectName AzV_266 -wlen 2.2 -band K -bright false -baseMax 102.45 -ra 01 01 09.42 -file AzV_266.vot -dec -72 27 28.40 -maxMagRange 13  "
