a3b40c158b64c6e84f1116b49ca22c660cc48612
gmella <guillaume.mella@univ-grenoble-alpes.fr>
2009-04-29 10:44:03 +0000

    add first implementation to compute orbit

--- sclcat/src/sclcatPrimaParseResult.sh
+++ sclcat/src/sclcatPrimaParseResult.sh
@@ -2,11 +2,14 @@
 #*******************************************************************************
 # JMMC project
 #
-# "@(#) $Id: sclcatPrimaParseResult.sh,v 1.8 2009-04-27 11:57:51 mella Exp $"
+# "@(#) $Id: sclcatPrimaParseResult.sh,v 1.9 2009-04-29 10:44:03 mella Exp $"
 #
 # History
 # -------
 # $Log: not supported by cvs2svn $
+# Revision 1.8  2009/04/27 11:57:51  mella
+# fix bug
+#
 # Revision 1.7  2009/04/27 08:58:39  mella
 # fix hardcoded values with dynamic accessor
 #
@@ -73,9 +76,12 @@ done
 
 XSLT_VOT2HTML="$PWD/../config/sclcatVOTable2html.xsl"
 XSLT_OBJECT2HTML="$PWD/../config/sclcatObjectsToHtml.xsl"
+XSLT_OBJECT2LATEX="$PWD/../config/sclcatObjectsToLatex.xsl"
 # Next file contains all grabed data
-MAIN_LIST_FILE="$PWD/../config/sclcatSimbadList.xml"
+SIMBAD_FILE="$PWD/../config/sclcatSimbadList.xml"
+ALIAS_FILE="$PWD/../config/sclcatAliases.xml"
 PRIMA_STAR_LIST="$PWD/../config/sclcatPrimaStars.xml"
+EXOPLANET_VOTABLE="$PWD/../config/sclcatPrimaExoplanetData.xml"
 
 # define temporary PATH # change it if the script becomes extern
 PATH=$PATH:$PWD/../bin
@@ -113,7 +119,20 @@ getColumnIndex()
 {
     VOTABLE_FILENAME="$1"
     COLNAME="$2"
-    xml sel -N VOT=http://www.ivoa.net/xml/VOTable/v1.1 -t  -m "//VOT:FIELD" -i "@name='$COLNAME'" -v "position()" "$VOTABLE_FILENAME"
+    VAL="$(xml sel -N VOT=http://www.ivoa.net/xml/VOTable/v1.1 -t  -m "//VOT:FIELD" -i "@name='$COLNAME'" -v "position()" "$VOTABLE_FILENAME")"
+    if [ -n "$VAL" ]
+    then
+        echo "$VAL"
+        return
+    fi
+    VAL="$(xml sel -N VOT=http://exoplanet.eu -t  -m "//VOT:FIELD" -i "@name='$COLNAME'" -v "position()" "$VOTABLE_FILENAME")"
+    if [ -n "$VAL" ]
+    then
+        echo "$VAL"
+        return
+    fi
+    return 1
+
 }
 
 getCellValue()
@@ -123,7 +142,19 @@ getCellValue()
     ROWINDEX="$3"
 
     COLINDEX=$(getColumnIndex "$VOTABLE_FILENAME" "$COLNAME")
-    xml sel -N VOT=http://www.ivoa.net/xml/VOTable/v1.1 -t -v "//VOT:TR[$ROWINDEX]/VOT:TD[$COLINDEX]" "$VOTABLE_FILENAME"
+    VAL=$(xml sel -N VOT=http://www.ivoa.net/xml/VOTable/v1.1 -t -v "//VOT:TR[$ROWINDEX]/VOT:TD[$COLINDEX]" "$VOTABLE_FILENAME")
+    if [ -n "$VAL" ]
+    then
+        echo "$VAL"
+        return
+    fi
+    VAL=$(xml sel -N VOT=http://exoplanet.eu -t -v "//VOT:TR[$ROWINDEX]/VOT:TD[$COLINDEX]" "$VOTABLE_FILENAME")
+    if [ -n "$VAL" ]
+    then
+        echo "$VAL"
+        return
+    fi
+    return 1
 }
 
 
@@ -142,14 +173,39 @@ for i in *.vot
 do
     if [ -f "$i" ]
     then
+        echo
         # Search indice of diamFlag column
         MULTFLAG_INDEX=$( getColumnIndex "$i" "diamFlag" )
+ 
+        # Search indexes into exoplanet votable
+        ST_NAME_INDEX=$(getColumnIndex "$EXOPLANET_VOTABLE" "St_Name")
+        ST_MASS_INDEX=$(getColumnIndex "$EXOPLANET_VOTABLE" "St_Mass")
+        ST_DIST_INDEX=$(getColumnIndex "$EXOPLANET_VOTABLE" "St_Dist")
+        PL_MASS_INDEX=$(getColumnIndex "$EXOPLANET_VOTABLE" "Pl_Mass")
+        PL_SEMIAXIS_INDEX=$(getColumnIndex "$EXOPLANET_VOTABLE" "Pl_SemiAxis")
+        PL_ECC_INDEX=$(getColumnIndex "$EXOPLANET_VOTABLE" "Pl_Ecc")
         
         starName=${i%.vot}
-        starRa=$(xml sel -t -v "//object[name='$starName']/ra" $MAIN_LIST_FILE)
-        starPmRa=$(xml sel -t -v "//object[name='$starName']/pmra" $MAIN_LIST_FILE)
-        starDec=$(xml sel -t -v "//object[name='$starName']/dec" $MAIN_LIST_FILE)
-        starPmDec=$(xml sel -t -v "//object[name='$starName']/pmdec" $MAIN_LIST_FILE)
+        OUT="$(xml sel -N VOT=http://exoplanet.eu -t -v "count(//VOT:TR[./VOT:TD[$ST_NAME_INDEX]='$starName'])" "$EXOPLANET_VOTABLE" )"
+        if [ "$OUT" -ge 1 ]
+        then
+            exoplanetStarName="$starName"
+        else
+            exoplanetStarName=$(xml sel -t -v "//object[@alias='$starName']/@name" $ALIAS_FILE)
+            OUT="$(xml sel -N VOT=http://exoplanet.eu -t -v "count(//VOT:TR[./VOT:TD[$ST_NAME_INDEX]='$exoplanetStarName'])" "$EXOPLANET_VOTABLE" )"
+            if [ "$OUT" -lt 1 ]
+            then
+                echo
+                echo "Can't find star into exoplanet votable using following names $starName $exoplanetStarName"
+                echo
+            fi
+        fi
+        
+
+        starRa=$(xml sel -t -v "//object[name='$starName']/ra" $SIMBAD_FILE)
+        starPmRa=$(xml sel -t -v "//object[name='$starName']/pmra" $SIMBAD_FILE)
+        starDec=$(xml sel -t -v "//object[name='$starName']/dec" $SIMBAD_FILE)
+        starPmDec=$(xml sel -t -v "//object[name='$starName']/pmdec" $SIMBAD_FILE)
         
         # if pmra or pmdec not found set it to 0
         if [ -z "$starPmRa" ]
@@ -161,6 +217,19 @@ do
             starPmDec=0
         fi
         
+        echo "<star simbadName=\"$starName\" scCount=\"$NBCAL\">" >>  $CALIBRATORS
+        xml sel -t -m "//object[name='$starName']" -e "ra" -v "ra" -b -e "dec" -v "dec" -b -e "pmra" -v "pmra" -b -e "pmdec" -v "pmdec" -b $SIMBAD_FILE >> $CALIBRATORS
+
+        # compute orbit size
+       ST_ARG="$(xml sel -N VOT=http://exoplanet.eu -t -m "//VOT:TR[./VOT:TD[$ST_NAME_INDEX]='$exoplanetStarName']" -i "position()=1" -o "&quot;" -v "./VOT:TD[$ST_MASS_INDEX]" -o "&quot; &quot;" -v "./VOT:TD[$ST_DIST_INDEX]" -o "&quot; " "$EXOPLANET_VOTABLE" )"
+        PL_ARGS="$(xml sel -N VOT=http://exoplanet.eu -t -m "//VOT:TR" -i "./VOT:TD[$ST_NAME_INDEX]='$exoplanetStarName'" -o "&quot;" -v "./VOT:TD[$PL_MASS_INDEX]" -o "&quot; &quot;" -v "./VOT:TD[$PL_SEMIAXIS_INDEX]" -o "&quot; &quot;" -v "./VOT:TD[$PL_ECC_INDEX]" -o "&quot; " -n "$EXOPLANET_VOTABLE" )"
+        ORBIT=$(eval sclcatComputeOrbit $ST_ARG $PL_ARGS)
+        echo "<!-- sclcatComputeOrbit $ST_ARG $PL_ARGS -->">> $CALIBRATORS
+        echo $ORBIT >> $CALIBRATORS
+        echo "sclcatComputeOrbit $ST_ARG $PL_ARGS "
+        echo $ORBIT 
+        
+        
         echo -n "$starName  "
         xml sel   -N VOT=http://www.ivoa.net/xml/VOTable/v1.1 -t \
         -v "count(//VOT:TR[./VOT:TD[$MULTFLAG_INDEX]='OK'])" -o "/" \
@@ -169,15 +238,13 @@ do
         NBCAL=$(xml sel   -N VOT=http://www.ivoa.net/xml/VOTable/v1.1 -t \
         -v "count(//VOT:TR)" "$i")
 
+
         # output copy of calibrator
         INDICES=$(xml sel -N VOT=http://www.ivoa.net/xml/VOTable/v1.1 -t \
         -m "//VOT:TR" -i "./VOT:TD[$MULTFLAG_INDEX]='OK'" \
         -v "position()" -o " " "$i")
         
-        echo "<star simbadName=\"$starName\" scCount=\"$NBCAL\">" >>  $CALIBRATORS
-        xml sel -t -m "//object[name='$starName']" -e "ra" -v "ra" -b -e "dec" -v "dec" -b -e "pmra" -v "pmra" -b -e "pmdec" -v "pmdec" -b $MAIN_LIST_FILE >> $CALIBRATORS
-
-        for index in $INDICES
+                for index in $INDICES
         do
             calName=$(getCellValue "$i" 2MASS $index )
             calMagK=$(getCellValue "$i" K $index )
@@ -229,11 +296,19 @@ echo "</calibrators>" >> $CALIBRATORS
 
 
 #Now CALIBRATORS file can be presented by next stylesheet
-echo "Html resume generated"
+OUTPUT_FILE=index.html
+echo "Html resume generated into $PWD/$OUTPUT_FILE"
 # generate the html view
-xsltproc  --path ./html:.:.. -o index.html --stringparam calibratorsFilename \
-          $CALIBRATORS --stringparam mainFilename $MAIN_LIST_FILE \
+xsltproc  --path ./html:.:.. -o "$OUTPUT_FILE" --stringparam calibratorsFilename \
+          $CALIBRATORS --stringparam mainFilename $SIMBAD_FILE \
           $XSLT_OBJECT2HTML $PRIMA_STAR_LIST
+OUTPUT_FILE=table.tex
+echo "Latex table generated into $PWD/$OUTPUT_FILE"
+# generate the html view
+xsltproc  --path ./html:.:.. -o "$OUTPUT_FILE" --stringparam calibratorsFilename \
+          $CALIBRATORS --stringparam mainFilename $SIMBAD_FILE \
+          $XSLT_OBJECT2LATEX $PRIMA_STAR_LIST
+
 cp -v $PRIMA_STAR_LIST .
-cp -v $MAIN_LIST_FILE .
+cp -v $SIMBAD_FILE .
 #___oOo___
