0d6e559b73d970fcfd3d2393bf2a202f6b5a6603
gmella <guillaume.mella@univ-grenoble-alpes.fr>
2009-04-30 13:07:33 +0000

    fix output of latexValue

--- sclcat/src/sclcatComputeOrbit.py
+++ sclcat/src/sclcatComputeOrbit.py
@@ -21,20 +21,25 @@ def getRadius(pmra, pmdec, timespan):
     dkal=vitsky*timespan+1.0
     return dkal
 
-def latexValue(number,dim=2):
+def latexValue(number,precision=2):
     """ Returns the best frm that we can accept as latex number value"""
     s=str(number)
+    if "." in s and s.index(".")==1 and not("e" in s) and not("0.0" in s):
+        return s[0:(2+precision)]
     # shorten too long decimals
-    if len(s)>5:
-        format="%." + "%d" % dim + "g"
+    if "." in s and len(s)>4:
+        format="%." + "%d" % precision + "E"
         s=format%number
     # shorten too long integers
     if not( "." in s ):
-        s="%g"%number
-    if "e" in s:
-        array=s.split("e")
-        format="\ee{%." + "%d" % (dim-1) + "f}{%d}"
-        s=format%(float(array[0]),int(array[1]))
+        s="%E"%number
+    if "E" in s:
+        array=s.split("E")
+        format="\ee{%." + "%d" % (precision-1) + "f}{%d}"
+        exp=int(array[1])
+        s=format%(float(array[0]),exp)
+        if exp==0:
+            s=str(number)[0:precision+2]
     return s
     
 
@@ -49,7 +54,7 @@ if __name__ == '__main__':
   
     if options.test:
         idx=0;
-        for i in [105,1050,10500,105000,1050000, 1.2,1.00,1.0001,1.0001001, 0.1, .12,.000001,.00000123,.00000167]:
+        for i in [105,1050,10500,105000,1050000, 1.2, 1.2345 ,1.00,1.0001,1.0001001, 0.52, 0.1, .12,.000001,.00000123,.00000167, 0.000492]:
             idx=idx+1
             print(idx)
             print("inputValue: ")
@@ -80,4 +85,4 @@ if __name__ == '__main__':
         o+=Pl_Mass/St_Mass*1/1.047355*St_Parax*Pl_SemiAxis*(1+Pl_Ecc)
    
 
-    print ("""<orbit latex="%s">%.2E</orbit>""" % (latexValue(o),o))
+    print ("""<orbit latex="%s" original="%f">%.2E</orbit>""" % (latexValue(o),o,o))
