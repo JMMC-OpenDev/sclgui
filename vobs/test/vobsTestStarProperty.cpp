f37249f1fc4e10bdb21a97478e2bee89e871eb9a
gmella <guillaume.mella@univ-grenoble-alpes.fr>
2009-10-16 10:00:54 +0000

    add string tests

--- vobs/test/vobsTestStarProperty.cpp
+++ vobs/test/vobsTestStarProperty.cpp
@@ -1,14 +1,18 @@
 /*******************************************************************************
  * JMMC project
  *
- * "@(#) $Id: vobsTestStarProperty.cpp,v 1.1 2009-10-14 14:50:58 lafrasse Exp $"
+ * "@(#) $Id: vobsTestStarProperty.cpp,v 1.2 2009-10-16 10:00:54 mella Exp $"
  *
  * History
  * -------
  * $Log: not supported by cvs2svn $
+ * Revision 1.1  2009/10/14 14:50:58  lafrasse
+ * Added proper support of floating point value (without loosing presion with
+ * conversion from string representation) with accompagning tests.
+ *
  ******************************************************************************/
 
-static char *rcsId __attribute__ ((unused))="@(#) $Id: vobsTestStarProperty.cpp,v 1.1 2009-10-14 14:50:58 lafrasse Exp $"; 
+static char *rcsId __attribute__ ((unused))="@(#) $Id: vobsTestStarProperty.cpp,v 1.2 2009-10-16 10:00:54 mella Exp $"; 
 
 /* 
  * System Headers 
@@ -50,6 +54,18 @@ int main(int argc, char *argv[])
         property.GetValue(&value);
         mcsFLOAT delta = testingFloats[i] - value;
         cout << "value = '" << value << "' " << (testingFloats[i] == value ? "==" : "!=") << " '" << testingFloats[i] << "' (original value), delta = '" << delta << "'." << endl;
+        cout << "Sumary : " << property.GetSummaryString() << endl << endl;
+    }
+    char *testingStrings[]= {"1.234567890123456789", "987654321.123456789", "0.00000123"};
+
+    for (int i = 0; i < 3; i++)
+    {
+        property.SetValue(testingStrings[i], "String Test", (vobsCONFIDENCE_INDEX)0, mcsTRUE);
+        mcsFLOAT value = FP_NAN;
+        property.GetValue(&value);
+        mcsFLOAT f = atof(testingStrings[i]) ;
+        mcsFLOAT delta = f - value;
+        cout << "value = '" << value << "' " << (f == value ? "==" : "!=") << " '" << string(testingStrings[i]) << "' (original value), delta = '" << delta << "'." << endl;
         cout << "Sumary : " << property.GetSummaryString() << endl << endl;
     }
     
