996633b9c1443a1f8cd7635ca28c57d4e4e9dd7a
JMMC-OpenDev <jmmc-tech-group@jmmc.fr>
2009-12-01 15:58:15 +0000

    This commit was manufactured by cvs2svn to create tag 'SC_V4_1_2b7/sclgui'.

---
+++ sclgui/include/sclgui.h
@@ -0,0 +1,43 @@
+#ifndef sclgui_H
+#define sclgui_H
+/*******************************************************************************
+ * JMMC project
+ *
+ * "@(#) $Id: sclgui.h,v 1.5 2005-10-11 15:24:15 scetre Exp $"
+ *
+ * History
+ * -------
+ * $Log: not supported by cvs2svn $
+ ******************************************************************************/
+
+/**
+ * \file
+ * sclgui general header file.
+ */
+
+
+/*
+ * Local headers
+ */
+#include "sclgui.h"
+#include "sclguiCONTROLLER.h"
+#include "sclguiACTIONS_VIEW.h"
+#include "sclguiCALIBRATOR_LIST_MODEL.h"
+#include "sclguiCALIBRATOR_LIST_VIEW.h"
+#include "sclguiDISTANCE_FILTER_VIEW.h"
+#include "sclguiFILTER_LIST_MODEL.h"
+#include "sclguiFILTER_VIEW.h"
+#include "sclguiLUMINOSITY_FILTER_VIEW.h"
+#include "sclguiMAGNITUDE_FILTER_VIEW.h"
+#include "sclguiBUTTONS_SUBPANEL.h"
+#include "sclguiMULTIPLICITY_FILTER_VIEW.h"
+#include "sclguiREQUEST_MODEL.h"
+#include "sclguiREQUEST_VIEW.h"
+#include "sclguiSPECTRAL_TYPE_FILTER_VIEW.h"
+#include "sclguiVARIABILITY_FILTER_VIEW.h"
+#include "sclguiVISIBILITY_FILTER_VIEW.h"
+ 
+
+#endif /*!sclgui_H*/
+
+/*___oOo___*/
