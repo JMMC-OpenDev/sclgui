0acabf9cd021babcafef7abc19aa3059ba53adcc
JMMC-OpenDev <jmmc-tech-group@jmmc.fr>
2010-07-26 16:09:50 +0000

    This commit was manufactured by cvs2svn to create tag 'SC_V4_2_b3/sclgui'.

---
+++ sclgui/src/fr/jmmc/scalib/sclgui/SearchCalibrators.java
@@ -0,0 +1,294 @@
+/*******************************************************************************
+ * JMMC project
+ *
+ * "@(#) $Id: SearchCalibrators.java,v 1.27 2009-04-30 12:56:51 lafrasse Exp $"
+ *
+ * History
+ * -------
+ * $Log: not supported by cvs2svn $
+ * Revision 1.26  2009/02/17 15:44:31  lafrasse
+ * Updated comments.
+ *
+ * Revision 1.25  2008/10/16 12:39:33  lafrasse
+ * Changed finnish() to finish() to reflect JMCS API typo correction.
+ *
+ * Revision 1.24  2008/09/18 21:03:21  lafrasse
+ * Added Preference update notification triggering to fully finish GUI setup
+ * (TableCellColors).
+ *
+ * Revision 1.23  2008/09/10 22:37:25  lafrasse
+ * Moved away from MCS Logger to standard Java logger API.
+ * Moved to new JMCS APIs.
+ *
+ * Revision 1.22  2008/05/29 10:10:16  mella
+ * Accept null value for SearchCal Argument
+ *
+ * Revision 1.21  2008/05/19 15:39:29  lafrasse
+ * Updated to add preliminary support for the new JMCS application framework.
+ *
+ * Revision 1.20  2007/06/26 08:39:27  lafrasse
+ * Removed most TODOs by adding error handling through exceptions.
+ *
+ * Revision 1.19  2007/03/16 10:07:34  lafrasse
+ * Added support for instanciation and execution from ASPRO.
+ *
+ * Revision 1.18  2007/02/14 13:51:17  lafrasse
+ * Corrected path for resources.
+ *
+ * Revision 1.17  2007/02/13 15:34:39  lafrasse
+ * Jalopyzation.
+ *
+ * Revision 1.16  2007/02/13 13:58:44  lafrasse
+ * Moved sources from sclgui/src/jmmc into sclgui/src/fr and renamed packages
+ *
+ * Revision 1.15  2006/11/30 14:54:27  lafrasse
+ * Added support for hidden legend view on startup.
+ *
+ * Revision 1.14  2006/11/23 16:20:39  lafrasse
+ * Added different location (US, Fr, ...) handling for Double input (, vs .).
+ *
+ * Revision 1.13  2006/11/18 23:10:59  lafrasse
+ * Handled StatusBar move.
+ *
+ * Revision 1.12  2006/11/13 17:12:18  lafrasse
+ * Moved all file Open, Save, and Export code into CalibratorsModel.
+ * Moved to Action based management for File menu and Query buttons.
+ * Added preliminary file Param parsing.
+ * Code and documentation refinments.
+ *
+ * Revision 1.11  2006/11/08 22:25:00  lafrasse
+ * Implemented filtering algorithm.
+ *
+ * Revision 1.10  2006/10/16 14:26:00  lafrasse
+ * Added command line option parsing.
+ * Added log level setting from command line option.
+ *
+ * Revision 1.9  2006/08/04 16:35:43  lafrasse
+ * Added queryModel & filtersModel hooks in VirtualObservatory
+ *
+ * Revision 1.8  2006/07/28 08:30:43  mella
+ * Use mcs generic Resources class
+ *
+ * Revision 1.7  2006/07/18 13:08:39  lafrasse
+ * Jalopyzation
+ *
+ * Revision 1.6  2006/07/12 15:51:51  lafrasse
+ * Corrected package import to resolve a comilation problem with
+ * jmmc.mcs.util.StatusBar
+ *
+ * Revision 1.5  2006/07/12 14:29:39  lafrasse
+ * Added the status bar
+ *
+ * Revision 1.4  2006/07/11 11:11:57  mella
+ * Change instanciation order
+ *
+ * Revision 1.3  2006/06/30 07:57:58  lafrasse
+ * Removed default query parameters definition
+ *
+ * Revision 1.2  2006/04/07 08:41:03  mella
+ * Preferences singleton is accessed using Preferences.getInstance()
+ *
+ * Revision 1.1  2006/03/27 11:59:58  lafrasse
+ * Added new experimental Java GUI
+ *
+ ******************************************************************************/
+package fr.jmmc.scalib.sclgui;
+
+import fr.jmmc.mcs.gui.*;
+import fr.jmmc.mcs.log.*;
+
+import gnu.getopt.Getopt;
+
+import java.awt.event.ActionEvent;
+
+import java.net.URL;
+
+import java.util.*;
+import java.util.logging.*;
+
+import javax.swing.*;
+
+
+/**
+ * @file
+ * brief description of the program, which ends at this dot.
+ *
+ * @synopsis
+ * \e \<Command Name\> [\e \<param1\> ... \e \<paramN\>]
+ *                     [\e \<option1\> ... \e \<optionN\>]
+ *
+ * @param param1 : description of parameter 1
+ * @param paramN : description of parameter N
+ *
+ * \n
+ * @opt
+ * @optname option1 : description of option 1
+ * @optname optionN : description of option N
+ *
+ * \n
+ * @details
+ * OPTIONAL detailed description of the c main file follows here.
+ *
+ * @usedfiles
+ * OPTIONAL. If files are used, for each one, name, and usage description.
+ * @filename fileName1 :  usage description of fileName1
+ * @filename fileName2 :  usage description of fileName2
+ *
+ * \n
+ * @env
+ * OPTIONAL. If needed, environmental variables accessed by the program. For
+ * each variable, name, and usage description, as below.
+ * @envvar envVar1 :  usage description of envVar1
+ * @envvar envVar2 :  usage description of envVar2
+ *
+ * \n
+ * @warning OPTIONAL. Warning if any (software requirements, ...)
+ *
+ * \n
+ * @ex
+ * OPTIONAL. Command example if needed
+ * \n Brief example description.
+ * @code
+ * Insert your command example here
+ * @endcode
+ *
+ * @sa OPTIONAL. See also section, in which you can refer other documented
+ * entities. Doxygen will create the link automatically.
+ * @sa modcppOPERATION.C
+ *
+ * @bug OPTIONAL. Known bugs list if it exists.
+ * @bug Bug 1 : bug 1 description
+ *
+ * @todo OPTIONAL. Things to forsee list.
+ * @todo Action 1 : action 1 description
+ *
+ */
+public class SearchCalibrators extends App
+{
+    /** Logger */
+    private static final Logger _logger = Logger.getLogger(
+            "fr.jmmc.scalib.sclgui.SearchCalibrators");
+
+    /** Main application object used to perform the optionnal query received from ASPRO */
+    public static VirtualObservatory _vo = null;
+
+    /** Store the optionnal query received from ASPRO by constructor */
+    private String _query = null;
+
+    /**
+     * Launch the SearchCalibrators application.
+     *
+     * Create all objects needed by SearchCalibrators and plug event responding
+     * loop (Listener/Listenable, Observer/Observable) in.
+     *
+     * @param args command-line options.
+     */
+    public SearchCalibrators(String[] args)
+    {
+        super(args);
+    }
+
+    /**
+     * Launch the SearchCalibrators application, plus automatically perform the given query if any.
+     *
+     * @param args command-line options.
+     * @param query an XML formatted string containing an SCLSVR-like query.
+     */
+    public SearchCalibrators(String[] args, String query)
+    {
+        /* Launch application initialization with:
+         *    - execution delayed for further initilization;
+         *    - without splashscreen;
+         *    - without protection against current process killing by its father.
+         */
+        super(args, false, false, false);
+
+        // Store received query for later execution
+        _query = query;
+
+        // Perform received query if any
+        run();
+    }
+
+    /** Initialize application objects */
+    protected void init(String[] args)
+    {
+        // Set the default locale to custom locale (for Numerical Fields "." ",")
+        Locale.setDefault(new Locale("en", "US"));
+
+        // Set default resource
+        fr.jmmc.mcs.util.Resources.setResourceName(
+            "fr/jmmc/scalib/sclgui/Resources");
+
+        try
+        {
+            // Get preferences
+            Preferences preferences = Preferences.getInstance();
+
+            // Create a query model
+            QueryModel queryModel = new QueryModel();
+
+            // Create filters
+            FiltersModel filtersModel = new FiltersModel(queryModel);
+            FiltersView  filtersView  = new FiltersView(filtersModel);
+
+            // Create a calibrators model and attach it to a calibrators view
+            CalibratorsModel calibratorsModel = new CalibratorsModel(filtersModel);
+            CalibratorsView  calibratorsView  = new CalibratorsView(calibratorsModel);
+
+            filtersModel.addObserver(calibratorsModel);
+
+            // Link everything up
+            _vo = new VirtualObservatory(queryModel, calibratorsModel,
+                    filtersModel);
+
+            // Attach the query model to its query view
+            QueryView queryView = new QueryView(queryModel, _vo);
+
+            // Retrieve application preferences and attach them to their view
+            // (This instance must be instanciated after dependencies)
+            PreferencesView preferencesView = new PreferencesView();
+            StatusBar       statusBar       = new StatusBar();
+            // Show the user the app is been initialized
+            StatusBar.show("application initialization...");
+
+            // Build the main window
+            MainWindow window = new MainWindow(_vo, queryView, calibratorsView,
+                    preferencesView, filtersView, statusBar);
+
+            // Triggers all preferences observers notification to finnish GUI setup.
+            preferences.triggerObserversNotification();
+        }
+        catch (Exception ex)
+        {
+            _logger.log(Level.FINE, "Initialization error", ex);
+        }
+    }
+
+    /** Execute application body */
+    protected void execute()
+    {
+        // If a query was received (when instanciated by ASPRO)
+        if (_query != null)
+        {
+            // Launch the request
+            _vo.executeQuery(_query);
+        }
+    }
+
+    /** Handle operations before closing application */
+    protected boolean finish()
+    {
+        return _vo.canLostModifications();
+    }
+
+    /**
+     * Main.
+     */
+    public static void main(String[] args)
+    {
+        // Start application
+        SearchCalibrators searchCalibrators = new SearchCalibrators(args);
+    }
+}
+/*___oOo___*/
---
+++ sclgui/src/sclguiFILTER_LIST_MODEL.cpp
@@ -0,0 +1,574 @@
+/*******************************************************************************
+ * JMMC project
+ *
+ * "@(#) $Id: sclguiFILTER_LIST_MODEL.cpp,v 1.8 2006-03-03 15:28:17 scetre Exp $"
+ *
+ * History
+ * -------
+ * $Log: not supported by cvs2svn $
+ * Revision 1.7  2005/12/14 09:04:50  scetre
+ * Used new filter constructor
+ *
+ * Revision 1.6  2005/11/30 10:35:58  scetre
+ * Updated with new filter
+ *
+ * Revision 1.5  2005/11/29 10:39:51  gzins
+ * Chnaged vobsBASE_FILTER to vobsFILTER
+ *
+ * Revision 1.4  2005/11/24 15:16:14  scetre
+ * Used vobsBASE_FILTER instead of vobsFILTER
+ *
+ * Revision 1.3  2005/11/05 15:55:51  gzins
+ * Change Enabled and Disabled to Enable and Disable
+ *
+ * Revision 1.2  2005/10/18 12:52:48  lafrasse
+ * First code revue
+ *
+ * Revision 1.1  2005/10/11 15:24:15  scetre
+ * New class of MVC second generation added. Removed Obsolete class. Changed Class present in the two versions.
+ *
+ ******************************************************************************/
+
+/**
+ * @file
+ * Definition of sclguiFILTER_LIST_MODEL class.
+ */
+
+static char *rcsId __attribute__ ((unused))="@(#) $Id: sclguiFILTER_LIST_MODEL.cpp,v 1.8 2006-03-03 15:28:17 scetre Exp $"; 
+
+
+/* 
+ * System Headers 
+ */
+#include <iostream>
+using namespace std;
+
+
+/*
+ * MCS Headers 
+ */
+#include "mcs.h"
+#include "log.h"
+#include "err.h"
+
+
+/*
+ * SCALIB Headers 
+ */
+#include "vobs.h"
+#include "sclsvr.h"
+
+
+/*
+ * Local Headers 
+ */
+#include "sclguiFILTER_LIST_MODEL.h"
+#include "sclguiPrivate.h"
+
+
+/**
+ * Class constructor.
+ */
+sclguiFILTER_LIST_MODEL::sclguiFILTER_LIST_MODEL(const char* filterId):
+vobsFILTER_LIST(filterId),
+_variabilityFilter("Variability Filter"),
+    _multiplicityFilter("Multiplicity Filter"),
+    _magnitudeFilter("Magnitude Filter"),
+    _visibilityFilter("Visibility Filter"),
+    _distanceFilter("Distance Filter"),
+    _spectralTypeFilter("Spectral type Filter"),
+    _luminosityFilter("Luminosity Filter")
+{
+    Init();
+}
+
+/**
+ * Class destructor.
+ */
+sclguiFILTER_LIST_MODEL::~sclguiFILTER_LIST_MODEL()
+{
+    /*vobsFILTER *filter;
+    // Delete all filter of the list
+    for (unsigned int el = 0; el < Size(); el++)
+    {
+        filter = GetNextFilter((mcsLOGICAL)(el==0));
+        delete filter;
+    }*/
+}
+
+
+/*
+ * Public methods
+ */
+/**
+ * Initialize the model.
+ *
+ * Clear the list of the model, disable all filters exept variability and
+ * multiplicity (enabled by default because the soft should gave calibrators
+ * without variability and multiplicity).
+ *
+ * @return always mcsSUCCESS
+ */
+mcsCOMPL_STAT sclguiFILTER_LIST_MODEL::Init()
+{
+    logTrace("sclguiFILTER_LIST_MODEL::Init()");
+
+    // Add variability filter
+    Add(&_variabilityFilter, "Variability Filter");
+    SetFilterVariability(mcsTRUE);
+
+    // Add multiplicity filter
+    Add(&_multiplicityFilter, "Multiplicity Filter");
+    SetFilterMultiplicity(mcsTRUE);
+
+    // Add magnitude filter
+    Add(&_magnitudeFilter, "Magnitude Filter");
+
+    // Add visibility filter
+    Add(&_visibilityFilter, "Visibility Filter");
+
+    // Add distance filter
+    Add(&_distanceFilter, "Distance Filter");
+
+    // Add spectral type filter
+    Add(&_spectralTypeFilter, "Spectral Type Filter");
+
+    // Add luminosity filter
+    Add(&_luminosityFilter, "Luminosity Filter");
+    
+    return mcsSUCCESS;
+}
+
+/**
+ * Reset the filter list.
+ *
+ * \return mcsSUCCESS on successful completion. Otherwise mcsFAILURE is 
+ * returned.
+ */
+mcsCOMPL_STAT sclguiFILTER_LIST_MODEL::ResetFilters(void)
+{
+    logTrace("sclguiFILTER_LIST_MODEL::ResetFilters()");
+    
+    // Reset all filters of the list
+    if (Reset() == mcsFAILURE)
+    {
+        return mcsFAILURE;
+    }
+   
+    // Enable the ddefault filters (variability and visibility)
+    SetFilterVariability(mcsTRUE);
+    SetFilterMultiplicity(mcsTRUE);
+    
+    // Notify any attached views
+    if (NotifyViews() == mcsFAILURE)
+    { 
+        return mcsFAILURE;
+    }
+    
+    return mcsSUCCESS;
+}
+
+/**
+ * Enable or disable the magnitude filter.
+ *
+ * @param band magnitude band to be filtered
+ * @param value magnitude value to be filtered
+ * @param range magnitude range to be filtered
+ * @param enable logical flag specifying wether the filter should be enabled or
+ * disabled
+ *
+ * @return mcsSUCCESS on successful completion. Otherwise mcsFAILURE is 
+ * returned. 
+ */
+mcsCOMPL_STAT sclguiFILTER_LIST_MODEL::SetFilterMagnitude(mcsSTRING32  band,
+                                                          mcsFLOAT     value,
+                                                          mcsFLOAT     range,
+                                                          mcsLOGICAL   enable)
+{
+    logTrace("sclguiFILTER_LIST_MODEL::EnableFilterMagnitude()");
+    
+    // Get the magnitude filter from the list
+    vobsMAGNITUDE_FILTER *magFilter =
+        (vobsMAGNITUDE_FILTER *)GetFilter("Magnitude Filter");
+
+    // if the filter must be enabled
+    if (enable == mcsTRUE)
+    {
+        // Enable it
+        if (magFilter->Enable() == mcsFAILURE)
+        {
+            return mcsFAILURE;
+        }
+
+        // Set the filter parameters
+        if (magFilter->SetMagnitudeValue(band, value, range) ==
+            mcsFAILURE)
+        {
+            return mcsFAILURE;
+        }
+    }
+    else
+    {
+        // Disable the filter
+        if (magFilter->Disable() == mcsFAILURE)
+        {
+            return mcsFAILURE;
+        }
+    }
+
+    // Notify any attached views
+    if (NotifyViews() == mcsFAILURE)
+    { 
+        return mcsFAILURE;
+    }
+    
+    return mcsSUCCESS;
+}
+
+/**
+ * Enable or disable the distance filter.
+ *
+ * @param raValue right ascension of the science object in hms
+ * @param decValue declinaison of the science object in dms
+ * @param raRange right ascension range
+ * @param decRange declinaison range
+ * @param enable logical flag specifying wether the filter should be enabled or
+ * disabled
+ *
+ * @return mcsSUCCESS on successful completion. Otherwise mcsFAILURE is 
+ * returned. 
+ */
+mcsCOMPL_STAT sclguiFILTER_LIST_MODEL::SetFilterDistance(mcsSTRING32  raValue,
+                                                         mcsSTRING32  decValue,
+                                                         mcsFLOAT     raRange,
+                                                         mcsFLOAT     decRange,
+                                                         mcsLOGICAL   enable)
+{
+    logTrace("sclguiFILTER_LIST_MODEL::SetFilterDistance()");
+
+    // Get the distance filter from the list
+    vobsDISTANCE_FILTER *distanceFilter = 
+        (vobsDISTANCE_FILTER *)GetFilter("Distance Filter");
+
+    // if the filter must be enabled
+    if (enable == mcsTRUE)
+    {
+        // Enable it
+        if (distanceFilter->Enable() == mcsFAILURE)
+        {
+            return mcsFAILURE;
+        }
+
+        // Set the filter parameters
+        if (distanceFilter->SetDistanceValue(raValue, decValue, raRange,
+                                             decRange) == mcsFAILURE)
+        {
+            return mcsFAILURE;
+        }
+    }
+    else
+    {
+        // Disable the filter
+        if (distanceFilter->Disable() == mcsFAILURE)
+        {
+            return mcsFAILURE;
+        }
+    }
+
+    // Notify any attached views
+    if (NotifyViews() == mcsFAILURE)
+    { 
+        return mcsFAILURE;
+    }
+
+    return mcsSUCCESS;
+}
+
+/**
+ * Enable or disable the visibility filter.
+ *
+ * @param visMax the maximal accuracy
+ * @param enable logical flag specifying wether the filter should be enabled or
+ * disabled
+ *
+ * @return mcsSUCCESS on successful completion. Otherwise mcsFAILURE is 
+ * returned. 
+ */
+mcsCOMPL_STAT sclguiFILTER_LIST_MODEL::SetFilterVisibility(mcsFLOAT    visMax,
+                                                           mcsLOGICAL  enable)
+{
+    logTrace("sclguiFILTER_LIST_MODEL::SetFilterVisibility()");
+    
+    // Get the visibility filter from the list
+    sclsvrVISIBILITY_FILTER *visibilityFilter = 
+        (sclsvrVISIBILITY_FILTER *)GetFilter("Visibility Filter");
+   
+    // if the filter must be enabled
+    if (enable == mcsTRUE)
+    {
+        // Enable it
+        if (visibilityFilter->Enable() == mcsFAILURE)
+        {
+            return mcsFAILURE;
+        }
+    
+        // Set the filter parameter
+        if (visibilityFilter->SetVisibilityValue(visMax) == mcsFAILURE)
+        {
+            return mcsFAILURE;
+        }
+    }
+    else
+    {
+        // Disable the filter
+        if (visibilityFilter->Disable() == mcsFAILURE)
+        {
+            return mcsFAILURE;
+        }
+    }
+   
+    // Notify any attached views
+    if (NotifyViews() == mcsFAILURE)
+    { 
+        return mcsFAILURE;
+    }
+    
+    return mcsSUCCESS;
+}
+
+/**
+ * Enable or disable the spectral type filter.
+ *
+ * @param tempClassList a list of temperature class
+ * @param enable logical flag specifying wether the filter should be enabled or
+ * disabled
+ *
+ * @return mcsSUCCESS on successful completion. Otherwise mcsFAILURE is 
+ * returned. 
+ */
+mcsCOMPL_STAT sclguiFILTER_LIST_MODEL::
+SetFilterTemperatureClass(std::list<char *> tempClassList,mcsLOGICAL enable)
+{
+    logTrace("sclguiFILTER_LIST_MODEL::SetFilterSpectralType()");
+
+    // Get the spectral type filter from the list
+    vobsSPECTRAL_TYPE_FILTER *spectralTypeFilter = 
+        (vobsSPECTRAL_TYPE_FILTER *)GetFilter("Spectral Type Filter");
+
+    // if the filter must be enabled
+    if (enable == mcsTRUE)
+    {
+        // Enable it
+        if (spectralTypeFilter->Enable() == mcsFAILURE)
+        {
+            return mcsFAILURE;
+        }
+
+        // Set the filter parameter
+        if (spectralTypeFilter->SetSpectralType(tempClassList) == mcsFAILURE)
+        {
+            return mcsFAILURE;
+        }
+    }
+    else
+    {
+        // Disable the filter
+        if (spectralTypeFilter->Disable() == mcsFAILURE)
+        {
+            return mcsFAILURE;
+        }
+    }
+
+    // Notify any attached views
+    if (NotifyViews() == mcsFAILURE)
+    { 
+        return mcsFAILURE;
+    }
+
+    return mcsSUCCESS;
+}
+
+/**
+ * Enable or disable the luminosity filter.
+ *
+ * @param lumClassList a list of luminosity class
+ * @param enable logical flag specifying wether the filter should be enabled or
+ * disabled
+ *
+ * @return mcsSUCCESS on successful completion. Otherwise mcsFAILURE is 
+ * returned. 
+ */
+mcsCOMPL_STAT sclguiFILTER_LIST_MODEL::
+SetFilterLuminosityClass(std::list<char *> lumClassList,
+                         mcsLOGICAL enable)
+{
+    logTrace("sclguiFILTER_LIST_MODEL::SetFilterLuminosity()");
+
+    // Get the luminosity filter from the list
+    vobsLUMINOSITY_FILTER *luminosityFilter = 
+        (vobsLUMINOSITY_FILTER *)GetFilter("Luminosity Filter");
+
+    // if the filter must be enabled
+    if (enable == mcsTRUE)
+    {
+        // Enable it
+        if (luminosityFilter->Enable() == mcsFAILURE)
+        {
+            return mcsFAILURE;
+        }
+
+        // Set the filter parameter
+        if (luminosityFilter->SetLuminosity(lumClassList) == mcsFAILURE)
+        {
+            return mcsFAILURE;
+        }
+    }
+    else
+    {
+        // Disable the filter
+        if (luminosityFilter->Disable() == mcsFAILURE)
+        {
+            return mcsFAILURE;
+        }
+    }
+
+    // Notify any attached views
+    if (NotifyViews() == mcsFAILURE)
+    { 
+        return mcsFAILURE;
+    }
+
+    return mcsSUCCESS;
+}
+
+/**
+ * Enable or disable the variability filter.
+ *
+ * @param enable logical flag specifying wether the filter should be enabled or
+ * disabled
+ *
+ * @return mcsSUCCESS on successful completion. Otherwise mcsFAILURE is 
+ * returned. 
+ */
+mcsCOMPL_STAT sclguiFILTER_LIST_MODEL::SetFilterVariability(mcsLOGICAL enable)
+{
+    logTrace("sclguiFILTER_LIST_MODEL::SetFilterVariability()");
+
+    // Get the variability filter from the list
+    vobsVARIABILITY_FILTER *variabilityFilter = 
+        (vobsVARIABILITY_FILTER *)GetFilter("Variability Filter");
+
+    // if the filter must be enabled
+    if (enable == mcsTRUE)
+    {
+        // Enable it
+        if (variabilityFilter->Enable() == mcsFAILURE)
+        {
+            return mcsFAILURE;
+        }
+    }
+    else
+    {
+        // Disable the filter
+        if (variabilityFilter->Disable() == mcsFAILURE)
+        {
+            return mcsFAILURE;
+        }
+
+    }
+
+    // Notify any attached views
+    if (NotifyViews() == mcsFAILURE)
+    { 
+        return mcsFAILURE;
+    }
+
+    return mcsSUCCESS;
+}
+
+/**
+ * Enable or disable the multiplicity filter.
+ *
+ * @param enable logical flag specifying wether the filter should be enabled or
+ * disabled
+ *
+ * @return mcsSUCCESS on successful completion. Otherwise mcsFAILURE is 
+ * returned. 
+ */
+mcsCOMPL_STAT sclguiFILTER_LIST_MODEL::SetFilterMultiplicity(mcsLOGICAL enable)
+{
+    logTrace("sclguiFILTER_LIST_MODEL::SetFilterVariability()");
+
+    // Get the multiplicity filter from the list
+    vobsMULTIPLICITY_FILTER *multiplicityFilter = 
+        (vobsMULTIPLICITY_FILTER *)GetFilter("Multiplicity Filter");
+
+    // if the filter must be enabled
+    if (enable == mcsTRUE)
+    {
+        // Enable it
+        if (multiplicityFilter->Enable() == mcsFAILURE)
+        {
+            return mcsFAILURE;
+        }
+    }
+    else
+    {
+        // Disable the filter
+        if (multiplicityFilter->Disable() == mcsFAILURE)
+        {
+            return mcsFAILURE;
+        } 
+    }
+
+    // Notify any attached views
+    if (NotifyViews() == mcsFAILURE)
+    { 
+        return mcsFAILURE;
+    }
+
+    return mcsSUCCESS;
+}
+
+/**
+ * Disable the filter of the given name
+ *
+ * @param name the name of the filter to disable
+ *
+ * @return mcsSUCCESS on successful completion. Otherwise mcsFAILURE is 
+ * returned.
+ */
+mcsCOMPL_STAT sclguiFILTER_LIST_MODEL::DisableFilter(mcsSTRING32 name)
+{
+    logTrace("sclguiFILTER_LIST_MODEL::DisableFilter()");
+
+    // Get the filter from the list by its name
+    vobsFILTER *filter = GetFilter(name);
+    
+    // Disable the filter
+    if (filter->Disable() == mcsFAILURE)
+    {
+        return mcsFAILURE;
+    }
+
+    // Notify any attached views
+    if (NotifyViews() == mcsFAILURE)
+    { 
+        return mcsFAILURE;
+    }
+    
+    return mcsSUCCESS;
+}
+
+
+/*
+ * Protected methods
+ */
+
+
+/*
+ * Private methods
+ */
+
+
+/*___oOo___*/
