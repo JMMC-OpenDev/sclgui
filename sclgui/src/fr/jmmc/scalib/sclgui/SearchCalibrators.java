26c46cf6578bfd6dd454c9445a49f4bbfcc7e3a3
bourgesl <bourges.laurent@gmail.com>
2010-10-22 10:56:43 +0000

    fixed class javadoc use EDT to executeQuery in execute method

--- sclgui/src/fr/jmmc/scalib/sclgui/SearchCalibrators.java
+++ sclgui/src/fr/jmmc/scalib/sclgui/SearchCalibrators.java
@@ -1,11 +1,15 @@
 /*******************************************************************************
  * JMMC project
  *
- * "@(#) $Id: SearchCalibrators.java,v 1.32 2010-10-11 14:02:37 lafrasse Exp $"
+ * "@(#) $Id: SearchCalibrators.java,v 1.33 2010-10-22 10:56:43 bourgesl Exp $"
  *
  * History
  * -------
  * $Log: not supported by cvs2svn $
+ * Revision 1.32  2010/10/11 14:02:37  lafrasse
+ * Ensures that GUI construction is done in EDT.
+ * Added general exception trapping with MCSExceptionHandler.
+ *
  * Revision 1.31  2010/10/11 09:03:22  lafrasse
  * Added JMCS exception handler.
  *
@@ -120,58 +124,8 @@ import java.util.logging.Logger;
 import javax.swing.SwingUtilities;
 
 /**
- * @file
- * brief description of the program, which ends at this dot.
- *
- * @synopsis
- * \e \<Command Name\> [\e \<param1\> ... \e \<paramN\>]
- *                     [\e \<option1\> ... \e \<optionN\>]
- *
- * @param param1 : description of parameter 1
- * @param paramN : description of parameter N
- *
- * \n
- * @opt
- * @optname option1 : description of option 1
- * @optname optionN : description of option N
- *
- * \n
- * @details
- * OPTIONAL detailed description of the c main file follows here.
- *
- * @usedfiles
- * OPTIONAL. If files are used, for each one, name, and usage description.
- * @filename fileName1 :  usage description of fileName1
- * @filename fileName2 :  usage description of fileName2
- *
- * \n
- * @env
- * OPTIONAL. If needed, environmental variables accessed by the program. For
- * each variable, name, and usage description, as below.
- * @envvar envVar1 :  usage description of envVar1
- * @envvar envVar2 :  usage description of envVar2
- *
- * \n
- * @warning OPTIONAL. Warning if any (software requirements, ...)
- *
- * \n
- * @ex
- * OPTIONAL. Command example if needed
- * \n Brief example description.
- * @code
- * Insert your command example here
- * @endcode
- *
- * @sa OPTIONAL. See also section, in which you can refer other documented
- * entities. Doxygen will create the link automatically.
- * @sa modcppOPERATION.C
- *
- * @bug OPTIONAL. Known bugs list if it exists.
- * @bug Bug 1 : bug 1 description
- *
- * @todo OPTIONAL. Things to forsee list.
- * @todo Action 1 : action 1 description
- *
+ * SearchCal application launcher
+ * @author sylvain lafrasse
  */
 public class SearchCalibrators extends App {
 
@@ -287,8 +241,17 @@ public class SearchCalibrators extends App {
     protected void execute() {
         // If a query was received (when instanciated by ASPRO)
         if (_query != null) {
-            // Launch the request
-            _vo.executeQuery(_query);
+
+            SwingUtilities.invokeLater(new Runnable() {
+
+                /**
+                 * Synchronized by EDT
+                 */
+                public void run() {
+                  // Launch the request
+                  _vo.executeQuery(_query);
+                }
+            });
         }
     }
 
