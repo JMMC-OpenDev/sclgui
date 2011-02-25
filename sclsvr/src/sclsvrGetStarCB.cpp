74fb4fb7f1d60661a76f6dac30625d20d8a9faac
JMMC-OpenDev <jmmc-tech-group@jmmc.fr>
2011-02-25 16:19:03 +0000

    This commit was manufactured by cvs2svn to create tag 'SC_V4_3/sclsvr'.

---
+++ sclsvr/src/sclsvrGetStarCB.cpp
@@ -0,0 +1,415 @@
+/*******************************************************************************
+ * JMMC project
+ *
+ * "@(#) $Id: sclsvrGetStarCB.cpp,v 1.37 2011-02-10 17:17:21 lafrasse Exp $"
+ *
+ * History
+ * -------
+ * $Log: not supported by cvs2svn $
+ * Revision 1.36  2010/11/10 15:45:16  lafrasse
+ * Decorelated GETSTAR from EVH callback mecanism.
+ *
+ * Revision 1.35  2010/09/23 19:18:41  mella
+ * add to getstar ability to export in votable
+ *
+ * Revision 1.34  2007/10/31 11:29:09  gzins
+ * Updated to use new sdbENTRY non-blocking class
+ *
+ * Revision 1.33  2007/06/27 13:00:59  scetre
+ * Do not removed science star if present in the resulting list.
+ * Updated get star command
+ *
+ * Revision 1.32  2007/05/15 08:37:16  gzins
+ * Fixed bug related to thread synchronisation
+ *
+ * Revision 1.31  2007/05/11 15:42:46  gzins
+ * Fixed bug; missing progressionMessage tread parameter setting
+ *
+ * Revision 1.30  2006/12/21 15:16:05  lafrasse
+ * Updated progression monitoring code (moved from static-based to instance-based).
+ *
+ * Revision 1.29  2006/10/26 08:15:57  gzins
+ * Renamed thrdTHREAD to thrdTHREAD_STRUCT
+ *
+ * Revision 1.28  2006/03/03 15:25:23  scetre
+ * Changed rcsId to rcsId __attribute__ ((unused))
+ *
+ * Revision 1.27  2006/02/23 16:08:21  lafrasse
+ * Added error stack purge in case of bad 'sdb' initialization
+ *
+ * Revision 1.26  2006/02/22 17:08:33  lafrasse
+ * Made it works even if there is no more semaphores left on the running system (no GUI progression in this case)
+ *
+ * Revision 1.25  2006/02/21 16:52:39  scetre
+ * Moved the 2 same method in one in sclsvrSERVER.cpp
+ * move the 2 same struct in sclsvrPrivate.h
+ *
+ * Revision 1.24  2005/12/22 14:39:38  scetre
+ * Added sdb and thrd in GetStar
+ *
+ * Revision 1.23  2005/11/21 13:51:47  scetre
+ * Changed bad scenario name
+ * Added scenario for single star research -> updated getStar
+ *
+ * Revision 1.22  2005/11/15 15:01:19  scetre
+ * Updated with new scenario structure
+ *
+ * Revision 1.21  2005/10/26 11:27:24  lafrasse
+ * Code review
+ *
+ * Revision 1.20  2005/06/01 14:18:54  scetre
+ * Added filters and filter list objects.
+ * Changed logExtDbg to logTrace
+ *
+ * Revision 1.19  2005/03/07 13:41:18  gzins
+ * Remove min baseline length
+ *
+ * Revision 1.18  2005/03/04 15:38:10  gzins
+ * Added timer log
+ *
+ * Revision 1.17  2005/02/08 20:57:22  gzins
+ * Updated call to SetPropertyValue()
+ *
+ * Revision 1.16  2005/02/08 04:39:32  gzins
+ * Updated for new vobsREQUEST API and used new sclsvrREQUEST class
+ *
+ * Revision 1.15  2005/02/07 14:38:45  gzins
+ * Changed GetLambda to GetWlen
+ *
+ * scetre    30-Nov-2004  Created
+ *
+ ******************************************************************************/
+
+/**
+ * @file
+ * sclsvrGetStarCB class definition.
+ */
+
+static char *rcsId __attribute__ ((unused))="@(#) $Id: sclsvrGetStarCB.cpp,v 1.37 2011-02-10 17:17:21 lafrasse Exp $"; 
+
+
+/* 
+ * System Headers 
+ */
+#include <iostream>
+using namespace std;
+
+
+/*
+ * MCS Headers 
+ */
+#include "mcs.h"
+#include "log.h"
+#include "err.h"
+#include "timlog.h"
+#include "thrd.h"
+#include "sdb.h"
+
+/*
+ * SCALIB Headers 
+ */
+#include "vobs.h"
+extern "C"{
+#include "simcli.h"
+}
+
+
+/*
+ * Local Headers 
+ */
+#include "sclsvrVersion.h"
+#include "sclsvrSERVER.h"
+#include "sclsvrPrivate.h"
+#include "sclsvrErrors.h"
+#include "sclsvrGETSTAR_CMD.h"
+#include "sclsvrCALIBRATOR_LIST.h"
+#include "sclsvrSCENARIO_BRIGHT_K.h"
+
+/*
+ * Local structure
+ */
+
+/*
+ * Public methods
+ */
+evhCB_COMPL_STAT sclsvrSERVER::GetStarCB(msgMESSAGE &msg, void*)
+{
+    logTrace("sclsvrSERVER::GetStarCB()");
+
+    miscoDYN_BUF dynBuf;
+    evhCB_COMPL_STAT complStatus;
+    complStatus = ProcessGetStarCmd(msg.GetBody(), dynBuf, &msg);
+
+    return complStatus;
+}
+
+/**
+ * Handle GETSTAR command.
+ * 
+ * It handles the given query corresponding to the parameter list of GETSTAR
+ * command, processes it and returns the result.
+ *
+ * @param query string containing request
+ * @param dynBuf dynamical buffer where result will be stored
+ *
+ * @return evhCB_NO_DELETE.
+ */
+mcsCOMPL_STAT sclsvrSERVER::GetStar(const char* query, miscoDYN_BUF &dynBuf)
+{
+    logTrace("sclsvrSERVER::GetStar()");
+
+    // Get calibrators
+    evhCB_COMPL_STAT complStatus;
+    complStatus = ProcessGetStarCmd(query, dynBuf, NULL);
+
+    // Update status to inform request processing is completed 
+    if (_status.Write("0") == mcsFAILURE)
+    {
+        return mcsFAILURE;
+    }
+
+    if (complStatus != evhCB_SUCCESS)
+    {
+        return mcsFAILURE;
+    }
+    return mcsSUCCESS;
+}
+
+/**
+ * GETSTAR command processing method.
+ *
+ * This method is called by GETSTAR command callback. It selects appropriated
+ * scenario, executes it and returns resulting list of calibrators
+ *
+ * @param query user query containing all command parameters in string format 
+ * @param dynBuf dynamical buffer where star data will be stored
+ * @param msg message corresponding to the received command. If not NULL, a
+ * thread is started and intermediate replies are sent giving the request
+ * processing status.
+ *
+ * @return Upon successful completion returns mcsSUCCESS. Otherwise,
+ * mcsFAILURE is returned.
+ */
+evhCB_COMPL_STAT sclsvrSERVER::ProcessGetStarCmd(const char* query, 
+                                                 miscoDYN_BUF &dynBuf, 
+                                                 msgMESSAGE* msg = NULL)
+{
+    logTrace("sclsvrSERVER::ProcessGetStarCmd()");
+
+    const char* cmdName = "GETSTAR";
+
+    // Search command
+    sclsvrGETSTAR_CMD getStarCmd(cmdName, query);
+    // Get the request as a string for the case of Save in VOTable
+    mcsSTRING256 requestString;
+    strncpy(requestString, cmdName, 256);
+
+    // Parse command
+    if (getStarCmd.Parse() == mcsFAILURE)
+    {
+        return evhCB_NO_DELETE | evhCB_FAILURE;
+    }
+    
+    // Start timer log
+    timlogDebugStart(cmdName);
+    thrdTHREAD_STRUCT monitorTask;
+    if (msg != NULL)
+    {
+        // Monitoring task parameters
+        sclsvrMONITOR_TASK_PARAMS monitorTaskParams;
+        monitorTaskParams.server  = this;
+        monitorTaskParams.message = msg;
+        monitorTaskParams.status  = &_status;
+    
+        // Monitoring task
+        monitorTask.function  = sclsvrMonitorTask;
+        monitorTask.parameter = (thrdFCT_ARG*)&monitorTaskParams;
+
+        // Launch the thread only if SDB had been succesfully started
+        if (thrdThreadCreate(&monitorTask) == mcsFAILURE)
+        {
+            return evhCB_NO_DELETE | evhCB_FAILURE;
+        }
+    }
+
+    // Get star name 
+    char* objectName;
+    if (getStarCmd.GetObjectName(&objectName) == mcsFAILURE)
+    {
+        return evhCB_NO_DELETE | evhCB_FAILURE;
+    }    
+
+    // Get filename 
+    char* fileName;
+    if (getStarCmd.GetFile(&fileName) == mcsFAILURE)
+    {
+        return evhCB_NO_DELETE | evhCB_FAILURE;
+    }    
+
+    // Get observed wavelength 
+    double wlen;
+    if (getStarCmd.GetWlen(&wlen) == mcsFAILURE)
+    {
+        return evhCB_NO_DELETE | evhCB_FAILURE;
+    }      
+
+    // Get baseline 
+    double baseline;
+    if (getStarCmd.GetWlen(&baseline) == mcsFAILURE)
+    {
+        return evhCB_NO_DELETE | evhCB_FAILURE;
+    }        
+    
+    // Get star position from SIMBAD
+    mcsSTRING32 ra, dec;
+    if (simcliGetCoordinates(objectName, ra, dec) == mcsFAILURE)
+    {
+        errAdd(sclsvrERR_STAR_NOT_FOUND, objectName, "SIMBAD");
+        return evhCB_NO_DELETE | evhCB_FAILURE;
+    }
+
+    // Prepare request to search information in other catalog
+    sclsvrREQUEST request;
+    if (request.SetObjectName(objectName) == mcsFAILURE)
+    {
+        return evhCB_NO_DELETE | evhCB_FAILURE;
+    }
+    if (request.SetFileName(fileName) == mcsFAILURE)
+    {
+        return evhCB_NO_DELETE | evhCB_FAILURE;
+    }
+    if (request.SetSearchBand("K") ==  mcsFAILURE)
+    {
+        return evhCB_NO_DELETE | evhCB_FAILURE;
+    }
+    if (request.SetObservingWlen(wlen) == mcsFAILURE)
+    {
+        return evhCB_NO_DELETE | evhCB_FAILURE;
+    }
+    if (request.SetMaxBaselineLength(baseline) ==  mcsFAILURE)
+    {
+        return evhCB_NO_DELETE | evhCB_FAILURE;
+    }
+
+    // Set star
+    vobsSTAR star;
+    star.SetPropertyValue(vobsSTAR_POS_EQ_RA_MAIN, ra, "");
+    star.SetPropertyValue(vobsSTAR_POS_EQ_DEC_MAIN, dec, "");
+    vobsSTAR_LIST starList;
+    starList.AddAtTail(star);
+    // init the scenario
+    if (_scenarioSingleStar.Init(&request, starList) == mcsFAILURE)
+    {
+        return evhCB_NO_DELETE | evhCB_FAILURE;        
+    }
+
+    if (_virtualObservatory.Search(&_scenarioSingleStar, request,
+                                   starList) == mcsFAILURE)
+    {
+        return evhCB_NO_DELETE | evhCB_FAILURE;
+    }
+
+    // If the star has been found in catalog
+    if (starList.Size() == 0)
+    {
+        errAdd(sclsvrERR_STAR_NOT_FOUND, objectName, "CDS catalogs");
+    }
+    else
+    {
+        // Get first star of the list 
+        sclsvrCALIBRATOR calibrator(*starList.GetNextStar(mcsTRUE));
+
+        if (_status.Write("Done") == mcsFAILURE)
+        {
+            return evhCB_NO_DELETE | evhCB_FAILURE;
+        }
+        //
+        //       // Complete missing properties of the calibrator 
+        //       if (calibrator.Complete(request) == mcsFAILURE)
+        //       {
+        //           // Ignore error
+        //           errCloseStack(); 
+        //       }
+        //
+        // Prepare reply
+        int propIdx;
+        vobsSTAR_PROPERTY *property;
+        // Add property name
+        for (propIdx = 0; propIdx < calibrator.NbProperties(); propIdx++)
+        {
+            property = calibrator.GetNextProperty((mcsLOGICAL)(propIdx==0));
+            dynBuf.AppendString(property->GetName());
+            dynBuf.AppendString("\t");
+        }
+        dynBuf.AppendString("\n");
+        // Add property value
+        for (propIdx = 0; propIdx < calibrator.NbProperties(); propIdx++)
+        {
+            property = calibrator.GetNextProperty((mcsLOGICAL)(propIdx==0));
+            dynBuf.AppendString(property->GetValue());
+            dynBuf.AppendString("\t");
+        }
+        dynBuf.AppendString("\n");
+
+        // Send reply
+        if (msg != NULL)
+        {
+            msg->SetBody(dynBuf.GetBuffer());
+        }
+        else
+        {
+            printf("%s", dynBuf.GetBuffer());
+        }
+        dynBuf.Reset();
+
+        string xmlOutput;
+        //request.AppendParamsToVOTable(xmlOutput);
+        char* voHeader = "Produced by beta version of getStar (In case of problem, please report to jmmc-user-support@ujf-grenoble.fr)";
+        // Get the software name and version
+        mcsSTRING32 softwareVersion;
+        snprintf(softwareVersion, sizeof(softwareVersion), 
+                 "%s v%s", "sclsvr", sclsvrVERSION);
+
+        // If a filename has been given, store results as file
+        if (strcmp(request.GetFileName(), "") != 0)
+        {
+            // Save the list as a VOTable v1.1
+            if (starList.SaveToVOTable
+                (request.GetFileName(), voHeader, softwareVersion,
+                 requestString, xmlOutput.c_str()) == mcsFAILURE)
+            {
+                return evhCB_NO_DELETE | evhCB_FAILURE;
+            }
+        }
+
+        // Send reply
+        if (msg != NULL)
+        {
+            if (SendReply(*msg) == mcsFAILURE)
+            {
+                return evhCB_NO_DELETE | evhCB_FAILURE;
+            }
+            // Wait for the actionForwarder thread end
+            if (thrdThreadWait(&monitorTask) == mcsFAILURE)
+            {
+                return evhCB_NO_DELETE | evhCB_FAILURE;
+            }
+        }
+    }
+
+    if (msg != NULL)
+    {
+        // Wait for the actionForwarder thread end
+        if (thrdThreadWait(&monitorTask) == mcsFAILURE)
+        {
+            return evhCB_NO_DELETE | evhCB_FAILURE;
+        }
+    }
+
+    // Stop timer log
+    timlogStop(cmdName);
+
+    return evhCB_SUCCESS;
+}
+
+/*___oOo___*/
---
+++ sclsvr/test/sclsvrTestCALIBRATOR.cpp
@@ -0,0 +1,164 @@
+/*******************************************************************************
+ * JMMC project
+ *
+ * "@(#) $Id: sclsvrTestCALIBRATOR.cpp,v 1.2 2010-11-09 10:58:30 lafrasse Exp $"
+ *
+ * History
+ * -------
+ * $Log: not supported by cvs2svn $
+ * Revision 1.1  2007/10/31 11:38:45  gzins
+ * Moved from sclsvrTestCalibrator.cpp
+ *
+ ******************************************************************************/
+
+static char *rcsId __attribute__ ((unused))="@(#) $Id: sclsvrTestCALIBRATOR.cpp,v 1.2 2010-11-09 10:58:30 lafrasse Exp $"; 
+
+/* 
+ * System Headers 
+ */
+#include <iostream>
+using namespace std;
+
+
+/*
+ * MCS Headers 
+ */
+#include "mcs.h"
+#include "log.h"
+#include "err.h"
+#include "timlog.h"
+
+/*
+ * Local Headers 
+ */
+#include "sclsvrCALIBRATOR.h"
+#include "sclsvrCALIBRATOR_LIST.h"
+#include "sclsvrPrivate.h"
+#include "sclsvr.h"
+#include "vobs.h"
+
+#include <iostream>
+#include <algorithm>
+#include <map>
+using namespace std;
+
+/* function object to check the value of a map element
+ */
+template <class K, class V>
+class value_equals {
+  private:
+    V value;
+  public:
+    // constructor (initialize value to compare with)
+    value_equals (const V& v)
+     : value(v) {
+    }
+    // comparison
+    bool operator() (pair<const K, V> elem) {
+        return elem.second == value;
+    }
+};
+
+int main(int argc, char *argv[])
+{
+    mcsInit(argv[0]);
+
+    logSetStdoutLogLevel(logINFO);
+    timlogStart(MODULE_ID, logINFO, "98", "testCalibrator");
+    
+    // add property to a star
+    vobsSTAR star;
+    star.SetPropertyValue(vobsSTAR_ID_HIP, "25123", "perso");
+    star.SetPropertyValue(vobsSTAR_POS_EQ_RA_MAIN, "03 47 29.08", "perso");
+    star.SetPropertyValue(vobsSTAR_POS_EQ_DEC_MAIN, "+24 06 18.5", "perso");
+    star.SetPropertyValue(vobsSTAR_PHOT_JHN_K, "2.01", "perso");
+    // add property to a star
+    vobsSTAR star2;
+    star2.SetPropertyValue(vobsSTAR_ID_HIP, "25233", "perso");
+    star2.SetPropertyValue(vobsSTAR_POS_EQ_RA_MAIN, "03 45 01.32", "perso");
+    star2.SetPropertyValue(vobsSTAR_POS_EQ_DEC_MAIN, "+24 23 02.23", "perso");
+    star2.SetPropertyValue(vobsSTAR_PHOT_JHN_K, "2.01", "perso");
+    printf("STAR\n");
+    for (int el2 = 0; el2 < star.NbProperties(); el2++)
+    {
+        printf("%s\t",star.GetNextProperty((mcsLOGICAL)(el2==0))
+               ->GetName());
+    }
+    printf("\n");
+
+    for (int el = 0; el < star.NbProperties(); el++)
+    {
+        printf("%s\t",star.GetNextProperty((mcsLOGICAL)(el==0))
+               ->GetValue());
+    }
+    printf("\n");
+
+    // create a calibrator from a star
+    sclsvrCALIBRATOR calibrator(star);
+    printf("CALIBRATOR\n");
+    for (int el3 = 0; el3 < calibrator.NbProperties(); el3++)
+     {
+         printf("%s\t",calibrator.GetNextProperty((mcsLOGICAL)(el3==0))
+                ->GetName());
+     }
+    printf("\n");
+    
+    for (int el4 = 0; el4 < calibrator.NbProperties(); el4++)
+    {
+        printf("%s\t",calibrator.GetNextProperty((mcsLOGICAL)(el4==0))->GetValue());
+    }
+
+    printf("\n");
+    sclsvrCALIBRATOR calibrator2(star2);
+    sclsvrCALIBRATOR calibrator3(star2);
+    
+    sclsvrCALIBRATOR_LIST list1;
+    sclsvrCALIBRATOR_LIST list2;
+    list2.Copy(list1);
+   
+    list2.AddAtTail(calibrator2);
+    list2.AddAtTail(calibrator);
+    list2.AddAtTail(calibrator3);
+    printf("before filter\n");
+    list2.Display();
+    sclsvrCALIBRATOR_LIST listTest;
+
+    // create a list of filter
+    vobsFILTER_LIST filterList("Filter List");
+    vobsDISTANCE_FILTER distanceFilter("Distance Filter");
+    // add distance filter in the list
+    filterList.Add(&distanceFilter, "Distance Filter");
+   
+    vobsDISTANCE_FILTER *distanceFilterbis;
+    // get distance filter of the list
+    distanceFilterbis = (vobsDISTANCE_FILTER*)filterList.GetFilter("Distance Filter");
+    // Set as enable the filter
+    distanceFilterbis->Enable();
+    distanceFilterbis->SetDistanceValue("03 47 29.08", "+24 06 18.5", 0.1);
+  
+    // apply filter
+    filterList.Apply(&list2);
+    //distanceFilter.Apply(&list2);
+    //list2.FilterByDistanceSeparation( "03 47 29.08", "+24 06 18.5", 0.1, 0.1);
+    //list2.GetMaximalMagnitudeSeparation("K", 2.5, 0.1);
+    printf("filter list\n");
+    list2.Display();
+
+    sclsvrCALIBRATOR scienceObject;
+    scienceObject.SetPropertyValue(vobsSTAR_POS_EQ_RA_MAIN, "03 47 29.08", "perso");
+    scienceObject.SetPropertyValue(vobsSTAR_POS_EQ_DEC_MAIN, "+24 06 18.5", "perso");
+
+
+    printf("other display\n");
+    scienceObject.Display();    
+    printf("science object of the list : \n");
+    list2.GetScienceObject(scienceObject);
+    scienceObject.Display();
+    errCloseStack();
+    logInfo("Exiting ...");
+    exit(EXIT_SUCCESS);
+
+    
+}
+
+/*___oOo___*/
