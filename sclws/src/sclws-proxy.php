394f0f764dc3fd25f5324a31aa5fb166204e042c
gmella <guillaume.mella@univ-grenoble-alpes.fr>
2011-02-28 12:42:31 +0000

    Use 8078 as default port Return on error message to the soap client if the associated server doe not run

--- sclws/src/sclws-proxy.php
+++ sclws/src/sclws-proxy.php
@@ -6,11 +6,14 @@
  * SearchCal Proxy Service 
  * -----------------------
  *
- * "@(#) $Id: sclws-proxy.php,v 1.1 2010-11-04 08:49:34 mella Exp $"
+ * "@(#) $Id: sclws-proxy.php,v 1.2 2011-02-28 12:42:31 mella Exp $"
  *
  * History
  * -------
- * $Log: not supported by cvs2svn $ 
+ * $Log: not supported by cvs2svn $
+ * Revision 1.1  2010/11/04 08:49:34  mella
+ * First revision
+ * 
  *  
  *  By default the searchcal soap service responds on non standart http port.
  *  This script aims to be a proxy for searchcal webservice. 
@@ -29,7 +32,30 @@
 
 // URL of the SOAP server
 $url = $this_header['SOAPServer'];
-$url = "http://jmmc.fr:8079";
+$url = "http://jmmc.fr:8078";
+
+// soap error message returned if searchCal server does not seems to run 
+$soapErrorMsg = <<<EOM
+<?xml version='1.0' encoding='UTF-8'?>
+<SOAP-ENV:Envelope
+xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/"
+xmlns:xsi="http://www.w3.org/1999/XMLSchema-instance"
+xmlns:xsd="http://www.w3.org/1999/XMLSchema">
+<SOAP-ENV:Body>
+<SOAP-ENV:Fault>
+<faultcode xsi:type="xsd:string">SOAP-ENV:Client</faultcode>
+<faultstring xsi:type="xsd:string">
+The main SearchCal server is probably down now. 
+Please check again in a couple of minutes. 
+If the problem still occurs, please send one feedback report (Help menu).
+</faultstring>
+</SOAP-ENV:Fault>
+</SOAP-ENV:Body>
+</SOAP-ENV:Envelope>
+EOM;
+
+
+
 //Start the Curl session
 $session = curl_init($url);
 //Capture all data posted
@@ -48,10 +74,13 @@ header("Content-Type:text/xml");
 
 // Make the call
 $response = curl_exec($session);
-$header_size = curl_getinfo($session,CURLINFO_HEADER_SIZE);
-$result = substr($response, $header_size );
-
-echo $result;
+if($response != FALSE){
+  $header_size = curl_getinfo($session,CURLINFO_HEADER_SIZE);
+  $result = substr($response, $header_size );
+  echo $result;
+}else{
+  echo $soapErrorMsg;
+}
 
 curl_close($session);
 ?>
