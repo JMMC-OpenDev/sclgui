c0baf9dedf4b4ee5159dc3b2d9bfb87d4b3b6cec
bourgesl <bourges.laurent@gmail.com>
2011-04-01 10:02:25 +0000

    custom CommonsHTTPSender implementation to support query abort (i.e. close underlying socket) and avoid threads blocked on socket I/O operations

---
+++ sclgui/src/org/apache/axis/transport/http/HttpMethodThreadMap.java
@@ -0,0 +1,76 @@
+/*******************************************************************************
+ * JMMC project
+ *
+ * "@(#) $Id: HttpMethodThreadMap.java,v 1.1 2011-04-01 10:02:25 bourgesl Exp $"
+ *
+ * History
+ * -------
+ * $Log: not supported by cvs2svn $
+ *
+ */
+package org.apache.axis.transport.http;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import org.apache.axis.components.logger.LogFactory;
+import org.apache.commons.httpclient.HttpMethodBase;
+import org.apache.commons.logging.Log;
+
+/**
+ * This class holds the mapping thread (name) <=> HTTPMethod
+ *
+ * @author bourgesl
+ */
+public final class HttpMethodThreadMap {
+
+  /** Field log */
+  protected static Log log = LogFactory.getLog(HttpMethodThreadMap.class.getName());
+
+  /* members */
+  /** mapping thread <name => HTTPMethod (thread-safe) */
+  private final Map<String, HttpMethodBase> methodMap = new ConcurrentHashMap<String, HttpMethodBase>();
+
+  /**
+   * Protected constructor
+   */
+  protected HttpMethodThreadMap() {
+    super();
+  }
+
+  /**
+   * Return the HttpMethodBase instance associated to the given thread name
+   * @param threadName thread name
+   * @return HttpMethodBase instance or null if undefined
+   */
+  public HttpMethodBase get(final String threadName) {
+    final HttpMethodBase method = methodMap.get(threadName);
+    if (log.isDebugEnabled()) {
+      log.debug("HttpMethodThreadMap.getHttpMethod : " + threadName + " = " + method);
+    }
+    return method;
+  }
+
+  /**
+   * Define the HttpMethodBase instance associated to the given thread name
+   * @param threadName thread name
+   * @param method HttpMethodBase instance
+   */
+  public void set(final String threadName, final HttpMethodBase method) {
+    if (log.isDebugEnabled()) {
+      log.debug("HttpMethodThreadMap.setHttpMethod : " + threadName + " = " + method);
+    }
+    methodMap.put(threadName, method);
+  }
+
+  /**
+   * Remove the HttpMethodBase instance associated to the given thread name
+   * @param threadName thread name
+   * @return HttpMethodBase removed HttpMethodBase instance or null if not found
+   */
+  public final HttpMethodBase remove(final String threadName) {
+    if (log.isDebugEnabled()) {
+      log.debug("HttpMethodThreadMap.remove : " + threadName);
+    }
+    return methodMap.remove(threadName);
+  }
+}
