6c7855a3c8befc1d4fed0a4f6ebb6d736618be4e
bourgesl <bourges.laurent@gmail.com>
2011-10-04 15:56:27 +0000

    fixed wrong positives (memcheck)

--- sclws/test/testBright.sh
+++ sclws/test/testBright.sh
@@ -7,7 +7,7 @@ sclwsTestServer "-objectName ETA_TAU -mag NaN -diffRa 3600.0 -diffDec 1200.0 -ba
 done
 
 #faint:
-sclwsTestServer "-objectName hip 32617_FAINT -mag 5.01 -diffRa 3600.0 -diffDec 1200.0 -band K -minMagRange 3.01 -maxMagRange 7.01 -ra 06 48 19.08795 -dec -01 19 07.8207 -baseMax 102.45 -wlen 2.2 -radius 0.0 -bright false -noScienceStar false" > sclwsTestBand_K_FAINT_$1.log
+sclwsTestServer "-objectName hip 32617_FAINT -mag 5.01 -diffRa 3600.0 -diffDec 1200.0 -band K -minMagRange 3.01 -maxMagRange 7.01 -ra 06 48 19.08795 -dec -01 19 07.8207 -baseMax 102.45 -wlen 2.2 -radius 0.0 -bright false -noScienceStar true" > sclwsTestBand_K_FAINT_$1.log
 
 sclwsTestServer "-minMagRange 10 -mag 12.9 -objectName AzV_266 -wlen 2.2 -band K -bright false -baseMax 102.45 -ra 01:01:09.42 -dec -72:27:28.40 -maxMagRange 13" > sclwsTestBand_K_FAINT_AzV266_$1.log
 
