ac360eb148cf318d126379a51e9f1dcb4538ff6d
gmella <guillaume.mella@univ-grenoble-alpes.fr>
2011-10-11 14:07:52 +0000

    fix bug with simbad crossmatch for wds using a blacklist

--- sclcat/src/sclcatFilterCatalog.sh
+++ sclcat/src/sclcatFilterCatalog.sh
@@ -76,8 +76,14 @@ newStep()
 
 removeWdsSb9WithSimbadCrossMatch()
 {
-# Extract star identifiers and prepare one simbad batch script
-    if [ simbad.txt -ot $PREVIOUSCATALOG ]
+
+    # Remove black listed stars before simbad query
+    BLACKLISTPARAM=$(grep -v "#" $SIMBADBLACKLIST)
+    echo "Actual simbad blacklist: $BLACKLISTPARAM"
+    newStep "Remove blacklisted stars to avoid error querying simbad" stilts ${STILTS_JAVA_OPTIONS} tpipe cmd='select !matches(Name,\"^('$BLACKLISTPARAM')$\")' in=$PREVIOUSCATALOG out=$CATALOG ;
+
+    # Extract star identifiers and prepare one simbad batch script
+    if [ simbad.txt -ot "$PREVIOUSCATALOG" -o ! -e simbad.vot -o simbad.txt -ot $SIMBADBLACKLIST ]
     then
         echo "Querying simbad by script"
         stilts tcat icmd="keepcols Name" in=$PREVIOUSCATALOG out=names.csv
@@ -90,18 +96,32 @@ removeWdsSb9WithSimbadCrossMatch()
         cat simbad.txt | awk '{if(write==1)print;if(start==1)write=1;if ($1=="::data::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::")start=1;}' > simbad.vot
         stilts tcat omode=stats in=simbad.vot 
     fi
+   
+    # Check that size of input and ouptut list are the same
+    SIMBADCNT=$( stilts tcat omode=count in=simbad.vot )
+    pwd
+    ls $PREVIOUSCATALOG
+    CATCNT=$( stilts tcat omode=count in=$PREVIOUSCATALOG )
+    if [ "${SIMBADCNT/*:/}" != "${CATCNT/*:/}" ]
+    then
+        echo "WARNING: SIMBAD does not return the same stars number as given in input"
+        echo "  Please add following stars in '$SIMBADBLACKLIST'"
+        grep "Identifier not found in the database" simbad.txt
+        exit 1
+    fi
 
     newStep "Crossmatch with simbad votable" stilts ${STILTS_JAVA_OPTIONS} tmatch2 matcher='exact' values1='$0' values2='$0' join='all1' in1=$PREVIOUSCATALOG in2=simbad.vot out=$CATALOG ;
+    
     newStep "Add new column with first WDS_id" stilts ${STILTS_JAVA_OPTIONS} tpipe in=$PREVIOUSCATALOG out=$CATALOG cmd='addcol WDS_id matchGroup(ID_2a,\"WDS\ J([0-9+-]*)\")'
 
-    if [ ! -e "WDS.vot" ]
+    if [ "WDS.vot" -ot "$PREVIOUSCATALOG" ]
     then
         echo "Extract wds catalog";
 curl -o WDS.vot 'http://vizier.u-strasbg.fr/viz-bin/votable?-to=4&-from=-2&-this=-2&-out.max=unlimited&-out.form=VOTable&-order=I&-c=&-c.eq=J2000&-oc.form=dec&-c.r=+10&-c.u=arcsec&-c.geom=r&-out.add=_r&-out.add=_RA*-c.eq%2C_DE*-c.eq&-out.add=_RAJ%2C_DEJ&-sort=_r&-source=B%2Fwds%2Fwds&-out=WDS&WDS=&-out=Disc&Disc=&-out=Comp&Comp=&-out=Obs1&Obs1=&-out=pa1&pa1=&-out=sep1&sep1=&-out=sep2&sep2=&-out=mag1&mag1=&-out=mag2&mag2=&-out=DM&DM=&-out=Notes&Notes=&-out=n_RAJ2000&n_RAJ2000=&-out=RAJ2000&RAJ2000=&-out=DEJ2000&DEJ2000=&-meta.ucd=u&-ref=VIZ4cb6b0cb2a6e&-file=.&-meta=0&-meta.foot=1'
     fi
 
 # keep only  WDS with sep1 or sep2 <= 2
-    if [ ! -e "WDSToRemove.vot" ]
+    if [ "WDSToRemove.vot" -ot "WDS.vot" ]
     then
         echo "keep only  WDS with sep1 or sep2 <= 2"
         stilts tpipe in='WDS.vot' out='WDSToRemove.vot' cmd='select (sep1<=2\ ||\ sep2<=2)' cmd='keepcols WDS' cmd='colmeta -name WDS_ORIG WDS'
@@ -137,6 +157,8 @@ done
 # Define temporary PATH # change it if the script becomes extern
 PATH=$PATH:$PWD/../bin
 
+# Define global variables
+SIMBADBLACKLIST=$PWD/../config/sclcatSimbadBlackList.txt
 
 
 # Parse command-line parameters
@@ -158,10 +180,15 @@ echo "Stilts options:"
 echo "$STILTS_JAVA_OPTIONS"
 echo 
 
+echo "Using '$SIMBADBLACKLIST' file to get proper simbad script filtering"
+
 # Starting real job
 
 # Moving to the right directory
-cd "${dir}/result/"
+if ! cd "${dir}/result/"
+then
+  exit 1
+fi
 
 # First initialization
 let PHASE=0
@@ -204,7 +231,7 @@ case $FILTERING_STYLE in
         newStep "Rejecting stars with MultFlag=S" stilts ${STILTS_JAVA_OPTIONS} tpipe in=$PREVIOUSCATALOG  cmd='progress ; select !contains(\"\"+MultFlag,\"S\")' out=$CATALOG ;
         newStep "Adding a flag column for R provenance" stilts ${STILTS_JAVA_OPTIONS} tpipe in=$PREVIOUSCATALOG  cmd='progress ; addcol f_Rmag NULL_R.confidence?1:0' out=$CATALOG ;
         newStep "Adding a flag column for I provenance" stilts ${STILTS_JAVA_OPTIONS} tpipe in=$PREVIOUSCATALOG  cmd='progress ; addcol f_Imag NULL_I.confidence?1:0' out=$CATALOG ;
-        newStep "Adding the 'Name' column" stilts ${STILTS_JAVA_OPTIONS} tpipe in=$PREVIOUSCATALOG  cmd='progress ; addcol Name !equals(HIP,\"NaN\")?\"HIP\"+HIP:(!(NULL_TYC1||NULL_TYC2||NULL_TYC3)?\"TYC\"+TYC1+\"-\"+TYC2+\"-\"+TYC3:\"\")' out=$CATALOG ;
+        newStep "Adding the 'Name' column to use one simbad script" stilts ${STILTS_JAVA_OPTIONS} tpipe in=$PREVIOUSCATALOG  cmd='progress ; addcol Name !equals(HIP,\"NaN\")?\"HIP\"+HIP:!equals(HD,\"NaN\")?\"HD\"+HD:(!(NULL_TYC1||NULL_TYC2||NULL_TYC3)?\"TYC\"+TYC1+\"-\"+TYC2+\"-\"+TYC3:\"\"+RAJ2000+\"\ \"+DEJ2000)' out=$CATALOG ;
         
         # Columns deletion (to force output of computed values in 'sclsvr' over retrieved one in 'vobs')
         newStep "Removing unwanted column UDDK" stilts ${STILTS_JAVA_OPTIONS} tpipe in=$PREVIOUSCATALOG cmd='delcols "UDDK"' out=$CATALOG ;
@@ -227,6 +254,7 @@ case $FILTERING_STYLE in
             newStep "Rejecting stars without '${COLUMN_NAME}'" stilts ${STILTS_JAVA_OPTIONS} tpipe in=$PREVIOUSCATALOG  cmd="progress ; select !NULL_${COLUMN_NAME}" out=$CATALOG ;
         done
 
+        # Add special simbad filtering until wds and sbc9 coordinates fixes
         removeWdsSb9WithSimbadCrossMatch
         ;;
 
@@ -234,11 +262,8 @@ case $FILTERING_STYLE in
         #newStep "Removing stars with DEC < 40 " stilts ${STILTS_JAVA_OPTIONS} tpipe in=$PREVIOUSCATALOG cmd='progress; select "dmsToRadians(DEJ2000) < degreesToRadians(40)"' out=$CATALOG ;
         newStep "Removing star with RA=18 56 44.3 " stilts ${STILTS_JAVA_OPTIONS} tpipe in=$PREVIOUSCATALOG cmd='progress; select !equals(RAJ2000,\"18\ 56\ 44.3\")' out=$CATALOG ;
        
-        #
-        # Add special simbad filtering until wds and sbc9 coordinates fixes
-        #
-        newStep "Adding the 'Name' column to use one simbad script" stilts ${STILTS_JAVA_OPTIONS} tpipe in=$PREVIOUSCATALOG  cmd='progress ; addcol Name !equals(HIP,\"NaN\")?\"HIP\"+HIP:!equals(HD,\"NaN\")?\"HD\"+HD:(!(NULL_TYC1||NULL_TYC2||NULL_TYC3)?\"TYC\"+TYC1+\"-\"+TYC2+\"-\"+TYC3:\"\"+RAJ2000+\"\ \"+DEJ2000)' out=$CATALOG ;
 
+        # Add special simbad filtering until wds and sbc9 coordinates fixes
         removeWdsSb9WithSimbadCrossMatch
 
         newStep "Clean temporary columns" stilts ${STILTS_JAVA_OPTIONS} tpipe in=$PREVIOUSCATALOG out=$CATALOG cmd='delcols $380\ $381\ $382\ $383\ $384\ $385\ $386\ $387\ $388\ $389\ $390' #\ $391\ $392\ $393\ $394\ $395\ $396\ $397' #\ $398\ $399\ $400'
