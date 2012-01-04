a1dd5ed2afeb38da175c44a68ddd0d1a166d2275
gmella <guillaume.mella@univ-grenoble-alpes.fr>
2012-01-04 13:46:42 +0000

    add new test case reference files

---
+++ sclsvr/test/ref/results/TestBrightH.cmd
@@ -0,0 +1,1 @@
+sclsvrServer -noDate -noFileLine -v 3 GETCAL "-diffDec 1200 -minMagRange 0 -mag 4 -objectName TestBrightH -diffRa 3600 -wlen 1.68 -band H -baseMax 102.45 -maxMagRange 8 -file TestBrightH.vot -dec 00:00:00.000 -ra 00:00:00.000  "
