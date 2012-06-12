c9f5d6fa17f134c755d99e9007d35d36ac34117a
gmella <guillaume.mella@univ-grenoble-alpes.fr>
2012-06-12 15:22:07 +0000

    Tagging SearchCal in version SC_V4_4_2 from SC_V4_4_1

---
+++ sclsvr/include/sclsvrVersion.h
@@ -0,0 +1,30 @@
+#ifndef sclsvrVersion_H
+#define sclsvrVersion_H
+/*******************************************************************************
+ * JMMC project ( http://www.jmmc.fr ) - Copyright (C) CNRS.
+ ******************************************************************************/
+
+/**
+ * @file
+ * Definition of the software version.
+ */
+
+/* The following piece of code alternates the linkage type to C for all 
+functions declared within the braces, which is necessary to use the 
+functions in C++-code.
+*/
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Software version 
+#define sclsvrVERSION "4.0.2"
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif /*!sclsvrVersion_H*/
+
+/*___oOo___*/
---
+++ sclsvr/test/ref/results/TestFaintK.cmd
@@ -0,0 +1,1 @@
+sclsvrServer -noDate -noFileLine -v 3 GETCAL "-minMagRange 0 -file TestFaintK.vot -objectName TestFaintK -wlen 2.37 -band K -bright false -baseMax 102.45 -maxMagRange 8 -mag 4 -diffRa 3600 -diffDec 1200 -ra 00:00:00.000 -dec 00:00:00.000  "
