d414390940675ffb3105947452cabcdc88ace3a3
sly74fr <slymacdev@free.fr>
2012-08-29 12:22:44 +0000

    Automatically tagged SearchCal in version SC_V4_4_3b2 (mkfRepoUtil)

---
+++ sclsvr/src/sclsvrSCENARIO_BRIGHT_N.cpp
@@ -0,0 +1,86 @@
+/*******************************************************************************
+ * JMMC project ( http://www.jmmc.fr ) - Copyright (C) CNRS.
+ ******************************************************************************/
+
+/**
+ * \file
+ *  Definition of sclsvrSCENARIO_BRIGHT_N class.
+ */
+
+
+/* 
+ * System Headers 
+ */
+#include <iostream>
+using namespace std;
+
+/*
+ * MCS Headers 
+ */
+#include "mcs.h"
+#include "log.h"
+#include "err.h"
+
+/*
+ * Local Headers 
+ */
+#include "sclsvrSCENARIO_BRIGHT_N.h"
+#include "sclsvrPrivate.h"
+
+/**
+ * Class constructor
+ */
+sclsvrSCENARIO_BRIGHT_N::sclsvrSCENARIO_BRIGHT_N(sdbENTRY* progress): vobsSCENARIO(progress)
+{
+}
+
+/**
+ * Class destructor
+ */
+sclsvrSCENARIO_BRIGHT_N::~sclsvrSCENARIO_BRIGHT_N()
+{
+}
+
+/*
+ * Public methods
+ */
+
+/**
+ * Return the name of this scenario
+ * @return "BRIGHT_N"
+ */
+const char* sclsvrSCENARIO_BRIGHT_N::GetScenarioName()
+{
+    return "BRIGHT_N";
+}
+
+/**
+ * Initialize the BRIGHT N scenario
+ *
+ * @param request user request
+ *
+ * @return mcsSUCCESS on successful completion. Otherwise mcsFAILURE is
+ * returned
+ */
+mcsCOMPL_STAT sclsvrSCENARIO_BRIGHT_N::Init(vobsREQUEST* request)
+{
+    logTrace("sclsvrSCENARIO_BRIGHT_N::Init()");
+
+    // Clear the scenario
+    Clear();
+    _request.Copy(*request);
+
+    // Clear the list input and list output which will be used
+    _starList.Clear();
+
+    // No duplicates in the MIDI catalog:
+    if (AddEntry(vobsCATALOG_MIDI_ID, &_request, NULL, &_starList, vobsCOPY, NULL) == mcsFAILURE)
+    {
+        return mcsFAILURE;
+    }
+
+    return mcsSUCCESS;
+}
+
+
+/*___oOo___*/
---
+++ sclsvr/test/ref/results/TestBrightI.cmd
@@ -0,0 +1,1 @@
+sclsvrServer -noDate -noFileLine -v 3 GETCAL "-diffDec 1200 -minMagRange 0 -mag 4 -objectName TestBrightI -diffRa 3600 -wlen 0.87 -band I -baseMax 102.45 -maxMagRange 8 -file TestBrightI.vot -dec 00:00:00.000 -ra 00:00:00.000  "
