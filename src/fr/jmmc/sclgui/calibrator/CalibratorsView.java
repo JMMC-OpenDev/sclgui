06d99f185c60040d85595d3930a29bf54b19a6cf
bourgesl <bourges.laurent@gmail.com>
2012-10-16 15:27:03 +0000

    fixed imports

--- src/fr/jmmc/sclgui/calibrator/CalibratorsView.java
+++ src/fr/jmmc/sclgui/calibrator/CalibratorsView.java
@@ -3,7 +3,6 @@
  ******************************************************************************/
 package fr.jmmc.sclgui.calibrator;
 
-import fr.jmmc.jmcs.gui.component.SearchField;
 import fr.jmmc.jmcs.gui.action.RegisteredPreferencedBooleanAction;
 import fr.jmmc.jmcs.gui.action.RegisteredAction;
 
@@ -15,11 +14,6 @@ import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Insets;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.HierarchyBoundsListener;
 import java.awt.event.HierarchyEvent;
 import java.awt.print.PageFormat;
@@ -27,15 +21,9 @@ import java.awt.print.Printable;
 import java.awt.print.PrinterException;
 import java.util.Observable;
 import java.util.Observer;
-import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
-import javax.swing.JButton;
-import javax.swing.JCheckBox;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
