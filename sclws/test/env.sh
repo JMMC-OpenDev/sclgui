0322272595b773ddf082a124b96d84310859b3cc
gmella <guillaume.mella@univ-grenoble-alpes.fr>
2013-01-17 08:06:18 +0000

    Automatically tagged SearchCal in version SC_V4_5b1 from trunk  (mkfRepoUtil)

---
+++ sclws/test/env.sh
@@ -0,0 +1,25 @@
+#*******************************************************************************
+# JMMC project ( http://www.jmmc.fr ) - Copyright (C) CNRS.
+#*******************************************************************************
+export SCLWS_PORT_NB=6666
+export VOBS_VIZIER_URI="http://vizier.u-strasbg.fr"
+export VERBOSITY=3
+
+export http_proxy=
+export https_proxy=
+
+export SCLWS_CMD="sclwsServer -l 0 -v $VERBOSITY"
+
+echo "SCLWS CMD  = $SCLWS_CMD"
+echo "SCLWS PORT = $SCLWS_PORT_NB"
+
+export VG_PATH=/usr/bin/
+#export VG_PATH=/home/users/bourgesl/apps/valgrind/bin
+export TSAN_PATH=/home/users/bourgesl/apps/tsan/
+
+echo "valgrind path = $VG_PATH"
+echo "tsan path     = $TSAN_PATH"
+
+#export G_SLICE=always-malloc
+#export GLIBCXX_FORCE_NEW=1
+
