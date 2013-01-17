a732792284d500e6a2c1a4a66132123a150f7f74
gmella <guillaume.mella@univ-grenoble-alpes.fr>
2013-01-17 08:06:14 +0000

    Automatically tagged SearchCal in version SC_V4_5b1 from trunk  (mkfRepoUtil)

--- sclsvr/test/ref/results/HD_26933.cmd
+++
@@ -1,1 +0,0 @@
-sclsvrServer -noDate -noFileLine -v 3 GETCAL "-minMagRange 10 -mag 11.3 -objectName HD_26933 -wlen 2.2 -band K -bright false -baseMax 102.45 -ra 05:18:19.27 -file HD_26933.vot -dec -69:11:40.60 -maxMagRange 12  "
--- sclsvr/test/ref/results/TestBright1.cmd
+++
@@ -1,1 +0,0 @@
-sclsvrServer -noDate -noFileLine -v 3 GETCAL "-diffDec 1200 -minMagRange 0 -mag 4 -objectName TestBright1 -diffRa 3600 -wlen 2.37 -band 1 -baseMax 102.45 -maxMagRange 8 -file TestBright1.vot -dec 00:00:00.000 -ra 00:00:00.000  "
